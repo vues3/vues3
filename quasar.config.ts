5e5c8a2ae4583b69d31fb55231f75958ec553e37
Jerry Bruwes <jbruwes@gmail.com>
2024-04-14 13:51:14 +0200

    изменено:      quasar.config.ts

--- quasar.config.ts
+++ quasar.config.ts
@@ -1,58 +1,133 @@
 import path from "node:path";
 
+import type { PluginEntry } from "@quasar/app-vite/types/configuration/build";
+import type { QuasarElectronConfiguration } from "@quasar/app-vite/types/configuration/electron-conf";
+import type {
+  QuasarFonts,
+  QuasarIconSets,
+  QuasarLanguageCodesHolder,
+  QuasarPlugins,
+} from "quasar";
 import { configure } from "quasar/wrappers";
 
+/** @type {string[]} */
+const boot: string[] = ["main", "uno", "route"];
+
+/** @type {string[]} */
+const css: string[] = ["app.sass"];
+
+/** @type {(QuasarIconSets | QuasarFonts)[]} */
+const extras: (QuasarIconSets | QuasarFonts)[] = [
+  "mdi-v6",
+  "roboto-font",
+  "material-icons",
+];
+
+/**
+ * @type {{
+ *   [key: string]: string;
+ * }}
+ */
+const alias: {
+  [key: string]: string;
+} = {
+  "@": path.join(__dirname, "./src"),
+  "~": __dirname,
+};
+
+/** @type {"history"} */
+const vueRouterMode: "history" = "history";
+
+// /** @type {boolean} */
+// const polyfillModulePreload: boolean = true;
+
+/** @type {string} */
+const tsconfigPath: string = "tsconfig.vue-tsc.json";
+
+/** @type {object} */
+const vueTsc: object = { tsconfigPath };
+
+/** @type {string} */
+const lintCommand: string = 'eslint "./**/*.{js,ts,mjs,cjs,vue}"';
+
+/** @type {object} */
+const eslint: object = { lintCommand };
+
+/** @type {boolean} */
+const server: boolean = false;
+
+/** @type {PluginEntry[]} */
+const vitePlugins: PluginEntry[] = [
+  ["vite-plugin-checker", { vueTsc, eslint }, { server }],
+];
+
+/** @type {"terser"} */
+const minify: "terser" = "terser";
+
+/** @type {object} */
+const build: object = {
+  alias,
+  vueRouterMode,
+  // polyfillModulePreload,
+  vitePlugins,
+  minify,
+};
+
+/** @type {boolean} */
+const open: boolean = true;
+
+/** @type {object} */
+const devServer: object = { open };
+
+/** @type {keyof QuasarLanguageCodesHolder} */
+const lang: keyof QuasarLanguageCodesHolder = "ru";
+
+/** @type {(keyof QuasarPlugins)[]} */
+const plugins: (keyof QuasarPlugins)[] = ["Dialog", "Notify"];
+
+/** @type {object} */
+const framework: object = { lang, plugins };
+
+/** @type {object} */
+const cordova: object = {};
+
+/** @type {boolean} */
+const hideSplashscreen: boolean = true;
+
+/** @type {object} */
+const capacitor: object = { hideSplashscreen };
+
+/** @type {number} */
+const inspectPort: number = 5858;
+
+/** @type {"packager"} */
+const bundler: "packager" = "packager";
+
+/** @type {object} */
+const packager: object = {};
+
+/** @type {QuasarElectronConfiguration} */
+const electron: QuasarElectronConfiguration = {
+  inspectPort,
+  bundler,
+  packager,
+};
+
+/** @type {string[]} */
+const contentScripts: string[] = ["my-content-script"];
+
+/** @type {object} */
+const bex: object = { contentScripts };
+
 export default configure(() => ({
-  boot: ["main", "uno", "route"],
-  css: ["app.sass"],
-  extras: ["mdi-v6", "roboto-font", "material-icons"],
-  build: {
-    alias: {
-      "@": path.join(__dirname, "./src"),
-      "~": __dirname,
-    },
-    vueRouterMode: "history",
-    // polyfillModulePreload: true,
-    vitePlugins: [
-      [
-        "vite-plugin-checker",
-        {
-          vueTsc: {
-            tsconfigPath: "tsconfig.vue-tsc.json",
-          },
-          eslint: {
-            lintCommand: 'eslint "./**/*.{js,ts,mjs,cjs,vue}"',
-          },
-        },
-        { server: false },
-      ],
-    ],
-    minify: "terser",
-  },
-  devServer: { open: true },
-  framework: {
-    lang: "ru",
-    plugins: ["Dialog", "Notify"],
-  },
-  ssr: {
-    pwa: false,
-    prodPort: 3000,
-    middlewares: ["render"],
-  },
-  pwa: {
-    workboxMode: "GenerateSW",
-    injectPwaMetaTags: true,
-    swFilename: "sw.js",
-    manifestFilename: "manifest.json",
-    useCredentialsForManifestTag: false,
-  },
-  cordova: {},
-  capacitor: { hideSplashscreen: true },
-  electron: {
-    inspectPort: 5858,
-    bundler: "packager",
-    packager: {},
-    builder: { appId: "vues3" },
-  },
-  bex: { contentScripts: ["my-content-script"] },
+  boot,
+  css,
+  extras,
+  build,
+  devServer,
+  framework,
+  cordova,
+  capacitor,
+  electron,
+  bex,
 }));
