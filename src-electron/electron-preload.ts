1bd43d9f24e8f935325d4a5add988d4d8e01016a
Jerry Bruwes <jbruwes@gmail.com>
2024-06-07 16:39:27 +0200

    изменено:      bun.lockb 	изменено:      package.json 	изменено:      src-electron/electron-preload.ts

--- src-electron/electron-preload.ts
+++ src-electron/electron-preload.ts
@@ -1,4 +1,5 @@
 import { BrowserWindow } from "@electron/remote";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { contextBridge } from "electron";
 
 contextBridge.exposeInMainWorld("myWindowAPI", {
