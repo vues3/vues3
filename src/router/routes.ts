552aa5e84bfa6e875a930c56d85cf5ff2a6db22d
Jerry Bruwes <jbruwes@gmail.com>
2024-07-06 18:41:58 +0200

    modified:   src/router/routes.ts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,13 +1,13 @@
 export default [
-  ...["License", "Content", "Settings", "Home"].map((value) => ({
+  {
     children: [
       {
-        component: (): object => import(`pages/${value}Page.vue`),
-        name: value,
+        component: (): object => import("pages/HomePage.vue"),
+        name: "Home",
         path: "",
       },
     ],
     component: (): object => import("layouts/MainLayout.vue"),
-    path: value === "Home" ? "/:catchAll(.*)*" : `/${value.toLowerCase()}`,
-  })),
+    path: "/:catchAll(.*)*",
+  },
 ];
