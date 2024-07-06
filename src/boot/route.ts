a0581a4e2205e4c75c7e86f71c6750f676f3d8fb
Jerry Bruwes <jbruwes@gmail.com>
2024-07-06 18:41:44 +0200

    modified:   src/boot/route.ts

--- src/boot/route.ts
+++ src/boot/route.ts
@@ -1,20 +1,17 @@
 import type { Router } from "vue-router";
 
-import privateItems from "assets/private.json";
-import publicItems from "assets/public.json";
+import routes from "src/router/routes";
 import { bucket } from "stores/s3";
 
 export default ({ router }: { router: Router }) => {
-  const privateTo = privateItems.map((val) => val.to);
-  const publicTo = publicItems.map((val) => val.to);
-
+  const [route] = routes;
   router.beforeEach(({ path }, from, next) => {
-    if (
-      (bucket.value && !privateTo.includes(path)) ||
-      (!bucket.value && !publicTo.includes(path))
-    )
-      next("/");
-    else next();
-    if (path === "/") bucket.value = "";
+    if (`/${bucket.value}` === path) next();
+    else next("/");
+    if (path === "/") {
+      bucket.value = "";
+      router.clearRoutes();
+      router.addRoute(route);
+    }
   });
 };
