aa573f838df2a8465e43d03ddd99dab27d3c937f
Jerry Bruwes <jbruwes@gmail.com>
2024-07-14 13:27:32 +0200

    modified:   monolit/src/views/NotFoundView.vue

--- monolit/src/views/NotFoundView.vue
+++ monolit/src/views/NotFoundView.vue
@@ -1,9 +1,8 @@
 <template lang="pug">
 Head
   title Page Not Found
-.hero.h-full
-  .hero-content
-    .prose.text-center
-      .text-9xl 404
-      p Oops. Nothing here...
+.absolute.inset-0.grid.place-items-center
+  .text-center
+    .text-9xl 404
+    p Oops. Nothing here...
 </template>
