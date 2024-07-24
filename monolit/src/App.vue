936efd13c8a21805721125b8f15945a30d4589a9
Jerry Bruwes <jbruwes@gmail.com>
2024-07-24 17:35:43 +0200

    modified:   monolit/src/App.vue

--- monolit/src/App.vue
+++ monolit/src/App.vue
@@ -7,9 +7,7 @@ Head
   meta(:content="canonical", property="og:url", v-if="canonical")
   meta(:content="image", property="og:image", v-if="image")
   meta(:content="a.alt[0]", property="og:image:alt", v-if="a?.alt[0]")
-  // eslint-disable-next-line sonarjs/no-vue-bypass-sanitization
   link(:href="favicon", :key, rel="icon", type="image/svg+xml")
-  // eslint-disable-next-line sonarjs/no-vue-bypass-sanitization
   link(:href="canonical", rel="canonical", v-if="canonical")
   meta(
     :content="data.settings?.yandex",
@@ -56,8 +54,6 @@ watch(a, async (value) => {
   try {
     icon = iconExists(name) ? getIcon(name) : await loadIcon(name);
   } catch (err) {
-    const { message } = err as Error;
-    window.console.log(message);
     icon = getIcon("mdi:web");
   } finally {
     if (icon) {
