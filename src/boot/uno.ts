b5536d41d3103b4aa1c9cff7a6c6e6f594fb64bc
Jerry Bruwes <jbruwes@gmail.com>
2024-07-25 13:21:32 +0200

    modified:   src/boot/uno.ts

--- src/boot/uno.ts
+++ src/boot/uno.ts
@@ -1,8 +1,7 @@
 import "@unocss/reset/tailwind-compat.css";
 import initUnocssRuntime from "@unocss/runtime";
-import { autoPrefix } from "app/src/stores/defaults";
 import defaults from "app/uno.config";
 
 export default () => {
-  initUnocssRuntime({ autoPrefix, defaults });
+  initUnocssRuntime({ defaults });
 };
