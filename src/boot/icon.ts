e4a1516bcd59c3c5a963bfce7ab423b4d8ecb81c
Jerry Bruwes <jbruwes@gmail.com>
2024-07-29 14:57:58 +0200

    deleted:    src/boot/icon.ts

--- src/boot/icon.ts
+++
@@ -1,8 +0,0 @@
-import type { App } from "vue";
-
-import { Icon } from "@iconify/vue";
-
-export default ({ app }: { app: App }) => {
-  // eslint-disable-next-line vue/multi-word-component-names
-  app.component("Icon", Icon);
-};
