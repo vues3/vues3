fe50312d8a8a7c98794a0b90d91fce71e9f3cf6f
Jerry Bruwes <jbruwes@gmail.com>
2024-08-08 16:06:25 +0200

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,11 @@ q-layout(view="hHh Lpr lff")
         v-if="bucket"
       )
         q-list(padding)
-          q-item(@click="click", clickable, v-close-popup)
-            q-item-section(avatar)
-              q-avatar(color="primary", icon="settings", text-color="white")
-            q-item-section
-              q-item-label Settings
+          //- q-item(@click="click", clickable, v-close-popup)
+          //-   q-item-section(avatar)
+          //-     q-avatar(color="primary", icon="settings", text-color="white")
+          //-   q-item-section
+          //-     q-item-label Settings
           q-item(clickable, to="/", v-close-popup)
             q-item-section(avatar)
               q-avatar(color="primary", icon="logout", text-color="white")
@@ -35,12 +35,12 @@ q-layout(view="hHh Lpr lff")
 </template>
 
 <script setup lang="ts">
-import type { TSettings } from "stores/types";
+// import type { TSettings } from "stores/types";
 
-import VSettingsDialog from "components/VSettingsDialog.vue";
-import { useQuasar } from "quasar";
+// import VSettingsDialog from "components/VSettingsDialog.vue";
+// import { useQuasar } from "quasar";
 import { rightDrawer } from "stores/app";
-import { data } from "stores/data";
+// import { data } from "stores/data";
 import { bucket } from "stores/s3";
 
 declare global {
@@ -48,14 +48,14 @@ declare global {
     myWindowAPI: Record<string, () => void> | undefined;
   }
 }
-const $q = useQuasar();
-const component = VSettingsDialog;
-const click = () => {
-  const componentProps = {
-    ...(data.value?.settings ?? {}),
-  };
-  $q.dialog({ component, componentProps }).onOk((value: TSettings) => {
-    if (data.value) data.value.settings = value;
-  });
-};
+// const $q = useQuasar();
+// const component = VSettingsDialog;
+// const click = () => {
+//   const componentProps = {
+//     ...(data.value?.settings ?? {}),
+//   };
+//   $q.dialog({ component, componentProps }).onOk((value: TSettings) => {
+//     if (data.value) data.value.settings = value;
+//   });
+// };
 </script>
