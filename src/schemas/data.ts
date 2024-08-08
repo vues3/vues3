e686238a0949f8faabb9597f52ca263140b0a54d
Jerry Bruwes <jbruwes@gmail.com>
2024-08-08 16:07:39 +0200

    modified:   src/schemas/data.ts

--- src/schemas/data.ts
+++ src/schemas/data.ts
@@ -2,26 +2,17 @@ import type { JSONSchema } from "json-schema-to-ts";
 
 const $id = "urn:jsonschema:data";
 const additionalProperties = false;
-const content = {
-  default: [{}],
-  items: { $ref: "urn:jsonschema:view" },
-  type: "array",
-} as const;
 const properties = {
-  settings: { $ref: "urn:jsonschema:settings", default: {}, type: "object" },
+  content: {
+    default: [{}],
+    items: { $ref: "urn:jsonschema:view" },
+    type: "array",
+  },
 } as const;
 const type = "object";
-const plainData = {
+export default {
   $id,
   additionalProperties,
   properties,
   type,
 } as const satisfies JSONSchema;
-const Data = {
-  $id,
-  additionalProperties,
-  properties: { content, ...properties },
-  type,
-} as const satisfies JSONSchema;
-export { plainData };
-export default Data;
