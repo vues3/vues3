ca93ddb3506342e63d88d05898032208fdeb241f
Jerry Bruwes <jbruwes@gmail.com>
2024-08-14 16:44:34 +0200

    deleted:    src/boot/uno.ts

--- src/boot/uno.ts
+++
@@ -1,7 +0,0 @@
-import "@unocss/reset/tailwind-compat.css";
-import initUnocssRuntime from "@unocss/runtime";
-import defaults from "app/uno.config";
-
-export default () => {
-  initUnocssRuntime({ defaults });
-};
