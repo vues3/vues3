f01235255a36de86b68a35b02b8029f590f7300d
Jerry Bruwes <jbruwes@gmail.com>
2024-08-15 20:22:52 +0200

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,24 @@ q-layout(view="hHh Lpr lff")
         v-if="bucket"
       )
         q-list(padding)
-          q-item(@click="click", clickable, v-close-popup)
+          q-item(
+            @click="() => { click('VImportmapDialog'); }",
+            clickable,
+            v-close-popup
+          )
+            q-item-section(avatar)
+              q-avatar(color="primary", icon="map", text-color="white")
+            q-item-section
+              q-item-label Import Map
+          q-item(
+            @click="() => { click('VStyleDialog'); }",
+            clickable,
+            v-close-popup
+          )
             q-item-section(avatar)
               q-avatar(color="primary", icon="style", text-color="white")
             q-item-section
-              q-item-label Loader CSS
+              q-item-label CSS
           q-item(clickable, to="/", v-close-popup)
             q-item-section(avatar)
               q-avatar(color="primary", icon="logout", text-color="white")
@@ -35,17 +48,18 @@ q-layout(view="hHh Lpr lff")
 </template>
 
 <script setup lang="ts">
-import VLoaderDialog from "components/VLoaderDialog.vue";
+import "@unocss/reset/tailwind-compat.css";
 import { useQuasar } from "quasar";
 import { rightDrawer } from "stores/app";
 import { bucket } from "stores/s3";
 // eslint-disable-next-line import/no-unresolved
 import "virtual:uno.css";
-import "@unocss/reset/tailwind-compat.css";
 
 const $q = useQuasar();
-const component = VLoaderDialog;
-const click = () => {
+const click = async (name: string) => {
+  const { default: component } = (await import(
+    `src/components/${name}.vue`
+  )) as typeof import("*.vue");
   $q.dialog({ component });
 };
 </script>
