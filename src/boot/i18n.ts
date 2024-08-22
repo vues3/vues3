c1a44d87dc5c2153a0c406034fcff932d3a1bba3
Jerry Bruwes <jbruwes@gmail.com>
2024-08-22 19:43:40 +0200

    modified:   src/boot/i18n.ts

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,32 +1,9 @@
+import { Lang } from "quasar";
 import { boot } from "quasar/wrappers";
 import messages from "src/i18n";
 import { createI18n } from "vue-i18n";
 
-export type MessageLanguages = keyof typeof messages;
-// Type-define 'en-US' as the master schema for the resource
-export type MessageSchema = (typeof messages)["en-US"];
-
-// See https://vue-i18n.intlify.dev/guide/advanced/typescript.html#global-resource-schema-type-definition
-/* eslint-disable @typescript-eslint/no-empty-interface */
-declare module "vue-i18n" {
-  // define the locale messages schema
-  export interface DefineLocaleMessage extends MessageSchema {}
-
-  // define the datetime format schema
-  export interface DefineDateTimeFormat {}
-
-  // define the number format schema
-  export interface DefineNumberFormat {}
-}
-/* eslint-enable @typescript-eslint/no-empty-interface */
-
 export default boot(({ app }) => {
-  const i18n = createI18n({
-    legacy: false,
-    locale: "en-US",
-    messages,
-  });
-
-  // Set i18n instance on app
-  app.use(i18n);
+  const locale = Lang.getLocale() === "ru-RU" ? "ru-RU" : "en-US";
+  app.use(createI18n({ locale, messages }));
 });
