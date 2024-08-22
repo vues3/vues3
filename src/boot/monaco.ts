825257005d5e9db333e148110c107806e4b80e13
Jerry Bruwes <jbruwes@gmail.com>
2024-08-22 19:45:26 +0200

    modified:   src/boot/monaco.ts

--- src/boot/monaco.ts
+++ src/boot/monaco.ts
@@ -4,24 +4,26 @@ import HtmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
 import JsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
 import JsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
-window.MonacoEnvironment = {
-  getWorker(workerId, label) {
-    switch (label) {
-      case "css":
-      case "less":
-      case "scss":
-        return new CssWorker();
-      case "handlebars":
-      case "html":
-      case "razor":
-        return new HtmlWorker();
-      case "javascript":
-      case "typescript":
-        return new JsWorker();
-      case "json":
-        return new JsonWorker();
-      default:
-        return new EditorWorker();
-    }
-  },
+export default () => {
+  window.MonacoEnvironment = {
+    getWorker(workerId, label) {
+      switch (label) {
+        case "css":
+        case "less":
+        case "scss":
+          return new CssWorker();
+        case "handlebars":
+        case "html":
+        case "razor":
+          return new HtmlWorker();
+        case "javascript":
+        case "typescript":
+          return new JsWorker();
+        case "json":
+          return new JsonWorker();
+        default:
+          return new EditorWorker();
+      }
+    },
+  };
 };
