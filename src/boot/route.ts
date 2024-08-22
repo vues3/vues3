831803f6acd324dfa9e6109ef21ddc3d53232e55
Jerry Bruwes <jbruwes@gmail.com>
2024-08-22 19:46:44 +0200

    modified:   src/boot/route.ts

--- src/boot/route.ts
+++ src/boot/route.ts
@@ -1,9 +1,8 @@
-import type { Router } from "vue-router";
-
+import { boot } from "quasar/wrappers";
 import routes from "src/router/routes";
 import { bucket } from "stores/s3";
 
-export default ({ router }: { router: Router }) => {
+export default boot(({ router }) => {
   const [route] = routes;
   router.beforeEach(({ path }, from, next) => {
     if (`/${bucket.value}` === path) next();
@@ -14,4 +13,4 @@ export default ({ router }: { router: Router }) => {
       router.addRoute(route);
     }
   });
-};
+});
