fda044b94abc38104c7e379274ec64796c9e6b76
Jerry Bruwes <jbruwes@gmail.com>
2024-08-23 19:01:37 +0200

    modified:   src/components/VLinkDialog.vue

--- src/components/VLinkDialog.vue
+++ src/components/VLinkDialog.vue
@@ -15,7 +15,7 @@ q-dialog(@hide="onDialogHide", ref="dialogRef")
           div {{ prop.node.name }}
     q-separator
     q-card-actions.text-primary(align="right")
-      q-btn(@click="onDialogCancel", flat, label="Отмена")
+      q-btn(:label="t('cancel')", @click="onDialogCancel", flat)
       q-btn(@click="onDialogOK(the?.to)", flat, label="Ok")
 </template>
 <script setup lang="ts">
@@ -25,11 +25,13 @@ import { Icon } from "@iconify/vue";
 import { useDialogPluginComponent } from "quasar";
 import { data, views } from "stores/data";
 import { computed, onMounted, ref } from "vue";
+import { useI18n } from "vue-i18n";
 
 defineEmits([...useDialogPluginComponent.emits]);
 const { dialogRef, onDialogCancel, onDialogHide, onDialogOK } =
   useDialogPluginComponent();
 const selected = ref();
+const { t } = useI18n();
 const the = computed(() => views.value.find(({ id }) => id === selected.value));
 const nodes = computed(() => data.value as QTreeNode[]);
 onMounted(() => {
