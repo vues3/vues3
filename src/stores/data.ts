7b422d269c3c7b030177e6344048391cc6afa1d9
Jerry Bruwes <jbruwes@gmail.com>
2024-08-27 15:32:16 +0200

    modified:   src/stores/data.ts

--- src/stores/data.ts
+++ src/stores/data.ts
@@ -1,36 +1,61 @@
-import type { TView } from "app/src/stores/types";
+import type { TPage } from "app/src/stores/types";
 import type { Ref } from "vue";
 
 import { configurable, deep, flush } from "app/src/stores/defaults";
 import { validateData } from "app/src/stores/types";
 import { computed, ref, watch } from "vue";
 
-const getViews = (views: TView[]): TView[] =>
-  views.flatMap((element) => [element, ...getViews(element.children ?? [])]);
+const getPages = (pages: TPage[]): TPage[] =>
+  pages.flatMap((element) => [element, ...getPages(element.children ?? [])]);
 const index = {
-  get(this: TView) {
+  get(this: TPage) {
     return this.siblings.findIndex(({ id }) => this.id === id);
   },
 };
 const prev = {
-  get(this: TView) {
+  get(this: TPage) {
     return this.siblings[this.index - 1];
   },
 };
 const next = {
-  get(this: TView) {
+  get(this: TPage) {
     return this.siblings[this.index + 1];
   },
 };
+const $children = {
+  get(this: TPage) {
+    return this.children?.filter(({ enabled }) => enabled);
+  },
+};
+const $siblings = {
+  get(this: TPage) {
+    return this.siblings.filter(({ enabled }) => enabled);
+  },
+};
+const $index = {
+  get(this: TPage) {
+    return this.$siblings.findIndex(({ id }) => this.id === id);
+  },
+};
+const $prev = {
+  get(this: TPage) {
+    return this.$siblings[this.index - 1];
+  },
+};
+const $next = {
+  get(this: TPage) {
+    return this.$siblings[this.index + 1];
+  },
+};
 const branch = {
-  get(this: TView) {
+  get(this: TPage) {
     const ret = [this];
     while (ret[0].parent) ret.unshift(ret[0].parent);
     return ret;
   },
 };
 const path = {
-  get(this: TView) {
+  get(this: TPage) {
     return this.branch
       .slice(1)
       .map(({ name }) => name?.replaceAll(" ", "_") ?? "_")
@@ -38,28 +63,33 @@ const path = {
   },
 };
 const to = {
-  get(this: TView) {
+  get(this: TPage) {
     return (this.loc?.replaceAll(" ", "_") ?? this.path)
       .replace(/^\/?/, "/")
       .replace(/\/?$/, "/");
   },
 };
 const i = {
-  get(this: TView) {
+  get(this: TPage) {
     return this.icon && `i-${this.icon}`;
   },
 };
 const title = {
-  get(this: TView) {
+  get(this: TPage) {
     return this.header ?? this.name;
   },
 };
 const fixDeep = (
-  siblings: { configurable?: boolean; value: TView[] },
-  parent: { configurable?: boolean; value?: TView } = { value: undefined },
+  siblings: { configurable?: boolean; value: TPage[] },
+  parent: { configurable?: boolean; value?: TPage } = { value: undefined },
 ) => {
   siblings.value.forEach((value) => {
     Object.defineProperties(value, {
+      $children,
+      $index,
+      $next,
+      $prev,
+      $siblings,
       branch,
       i,
       index,
@@ -77,11 +107,11 @@ const fixDeep = (
     );
   });
 };
-export const data: Ref<TView[] | undefined> = ref();
-const get = () => getViews(data.value ?? []);
-export const views = computed(() =>
-  get().map((value: TView) => {
-    Reflect.defineProperty(value, "views", { get });
+export const data: Ref<TPage[] | undefined> = ref();
+const get = () => getPages(data.value ?? []);
+export const pages = computed(() =>
+  get().map((value: TPage) => {
+    Reflect.defineProperty(value, "pages", { get });
     return value;
   }),
 );
