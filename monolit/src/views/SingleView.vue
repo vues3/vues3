7cec92596f78618dd9a48033fc97262008cb22b5
Jerry Bruwes <jbruwes@gmail.com>
2024-08-27 15:29:21 +0200

    modified:   monolit/src/views/SingleView.vue

--- monolit/src/views/SingleView.vue
+++ monolit/src/views/SingleView.vue
@@ -3,13 +3,13 @@ div(:class="the?.class", :id, un-cloak, v-if="the?.enabled")
   component(:is="is", :the, @vue:mounted="() => { resolve(the); }")
 </template>
 <script setup lang="ts">
-import type { TView } from "src/stores/types";
+import type { TPage } from "src/stores/types";
 
 import { computed } from "vue";
 
 import { getAsyncComponent, resolve, that } from "../stores/monolit";
 
-const props = defineProps<{ the?: TView }>();
+const props = defineProps<{ the?: TPage }>();
 const the = computed(() => props.the ?? that.value);
 const id = computed(() => the.value?.id);
 const is = computed(() => the.value && getAsyncComponent(the.value));
