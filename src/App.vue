8c161660135a75293d333d4accb3e9368e52572e
Jerry Bruwes <jbruwes@gmail.com>
2024-09-02 18:50:55 +0200

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,24 @@ q-layout(view="hHh Lpr lff")
               q-avatar(color="primary", icon="map", text-color="white")
             q-item-section
               q-item-label Import Map
+          q-item(
+            @click="() => { click(VFaviconDialog); }",
+            clickable,
+            v-close-popup
+          )
+            q-item-section(avatar)
+              q-avatar(color="primary", icon="image", text-color="white")
+            q-item-section
+              q-item-label Favicon
+          q-item(
+            @click="() => { click(VRobotsDialog); }",
+            clickable,
+            v-close-popup
+          )
+            q-item-section(avatar)
+              q-avatar(color="primary", icon="android", text-color="white")
+            q-item-section
+              q-item-label Robots.txt
           q-item(clickable, to="/", v-close-popup)
             q-item-section(avatar)
               q-avatar(color="primary", icon="logout", text-color="white")
@@ -43,7 +61,9 @@ import type { Component } from "vue";
 
 import "@unocss/reset/tailwind-compat.css";
 import { useQuasar } from "quasar";
+import VFaviconDialog from "src/components/VFaviconDialog.vue";
 import VImportmapDialog from "src/components/VImportmapDialog.vue";
+import VRobotsDialog from "src/components/VRobotsDialog.vue";
 import { rightDrawer } from "stores/app";
 import { bucket } from "stores/s3";
 // eslint-disable-next-line import/no-unresolved
