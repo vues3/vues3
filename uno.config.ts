665190e01bc945777b3b914106dae1f3dc04562b
Jerry Bruwes <jbruwes@gmail.com>
2024-09-03 18:23:03 +0200

    modified:   uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,7 @@ import type { Preset } from "@unocss/core";
 import presetIcons from "@unocss/preset-icons/browser";
 import presetTypography from "@unocss/preset-typography";
 import presetUno from "@unocss/preset-uno";
-import presetWebFonts from "@unocss/preset-web-fonts";
+// import presetWebFonts from "@unocss/preset-web-fonts";
 
 /** @see {@link https://type.today/ru/journal/old-style} Динамические антиквы */
 import dynamicSerifs from "./src/assets/fonts/DynamicSerifs.json";
@@ -20,8 +20,16 @@ import staticSerifs from "./src/assets/fonts/StaticSerifs.json";
 /** @see {@link https://type.today/ru/journal/transitional} Переходные антиквы */
 import transitionalSerifs from "./src/assets/fonts/TransitionalSerifs.json";
 
-const fonts: Record<string, string> = Object.fromEntries(
+export const fonts: Record<string, string> = Object.fromEntries(
   [
+    "Arial",
+    "Arial Black",
+    "Comic Sans MS",
+    "Courier New",
+    "Impact",
+    "Lucida Grande",
+    "Times New Roman",
+    "Verdana",
     ...dynamicSerifs,
     ...geometricGrotesques,
     ...humanisticGrotesques,
@@ -31,13 +39,12 @@ const fonts: Record<string, string> = Object.fromEntries(
     ...handwrittenFonts,
   ].map((value) => [value.toLowerCase().replaceAll(" ", "_"), value]),
 );
-const customFetch = async (url: string) => (await fetch(url)).text();
+export const customFetch = async (url: string) => (await fetch(url)).text();
 const cdn = "https://esm.sh/";
 const presets: Preset[] = [
   presetUno(),
   presetTypography(),
-  presetWebFonts({ customFetch, fonts }),
+  // presetWebFonts({ customFetch, fonts }),
   presetIcons({ cdn }),
 ];
-export { fonts };
 export default { presets };
