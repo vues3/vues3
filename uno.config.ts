17e644475f2db75cf87ff9946766fd39fae7b359
Jerry Bruwes <jbruwes@gmail.com>
2024-09-09 14:46:44 +0200

    modified:   uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -4,7 +4,7 @@ import presetIcons from "@unocss/preset-icons/browser";
 import presetTypography from "@unocss/preset-typography";
 import presetUno from "@unocss/preset-uno";
 
-const cdn = "https://esm.sh/";
+const cdn = "https://unpkg.com/";
 const presets: Preset[] = [
   presetUno(),
   presetTypography(),
