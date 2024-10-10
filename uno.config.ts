99af7f9513b8d3aa38c9ce250ffdc551c5f5ffd4
Jerry Bruwes <jbruwes@gmail.com>
2024-10-10 16:09:46 +0200

    modified:   bun.lockb 	modified:   package.json 	modified:   uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,8 @@
 import type { Preset } from "@unocss/core";
 
+import presetAttributify from "@unocss/preset-attributify";
 import presetIcons from "@unocss/preset-icons/browser";
+import presetTagify from "@unocss/preset-tagify";
 import presetTypography from "@unocss/preset-typography";
 import presetUno from "@unocss/preset-uno";
 
@@ -9,5 +11,7 @@ const presets: Preset[] = [
   presetUno(),
   presetTypography(),
   presetIcons({ cdn }),
+  presetTagify(),
+  presetAttributify(),
 ];
 export default { presets };
