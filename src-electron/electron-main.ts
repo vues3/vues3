d258c76b9d11e1a36d82cea3befcd923c1c575ad
Jerry Bruwes <jbruwes@gmail.com>
2024-10-17 16:27:26 +0200

    modified:   src-electron/electron-main.ts

--- src-electron/electron-main.ts
+++ src-electron/electron-main.ts
@@ -1,24 +1,20 @@
+import { enable, initialize } from "@electron/remote/main/index.js";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { app, BrowserWindow, Menu } from "electron";
 import path from "path";
 import { fileURLToPath } from "url";
 
+initialize();
 Menu.setApplicationMenu(null);
 const currentDir = fileURLToPath(new URL(".", import.meta.url));
 let mainWindow: BrowserWindow | undefined;
 const icon = path.resolve(currentDir, "icons/icon.png");
 const devTools = false;
 const show = false;
-const preload = path.resolve(
-  currentDir,
-  path.join(
-    process.env.QUASAR_ELECTRON_PRELOAD_FOLDER,
-    `electron-preload${process.env.QUASAR_ELECTRON_PRELOAD_EXTENSION}`,
-  ),
-);
-const webPreferences = { devTools, preload };
+const webPreferences = { devTools };
 const createWindow = async () => {
   mainWindow = new BrowserWindow({ icon, show, webPreferences });
+  enable(mainWindow.webContents);
   if (process.env.DEV)
     await mainWindow.loadURL(process.env.APP_URL).catch(() => {});
   else await mainWindow.loadFile("index.html").catch(() => {});
