d680167cb89a8ba569c3ac262385c01eb449ce01
Jerry Bruwes <jbruwes@gmail.com>
2024-10-24 19:31:25 +0200

    modified:   src-electron/electron-preload.ts

--- src-electron/electron-preload.ts
+++ src-electron/electron-preload.ts
@@ -1,4 +1,10 @@
 import { dialog } from "@electron/remote";
 import { contextBridge } from "electron";
+import { lstatSync } from "fs";
 
+const throwIfNoEntry = false;
 contextBridge.exposeInMainWorld("dialog", dialog);
+contextBridge.exposeInMainWorld(
+  "isDirectory",
+  (path: string) => lstatSync(path, { throwIfNoEntry })?.isDirectory() ?? false,
+);
