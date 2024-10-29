5ec1a60ddbe451d0b10aa661edd85f0a017dea16
Jerry Bruwes <jbruwes@gmail.com>
2024-10-29 19:55:01 +0200

    modified:   src/components/VCredsDialog.vue

--- src/components/VCredsDialog.vue
+++ src/components/VCredsDialog.vue
@@ -3,17 +3,29 @@ q-dialog(@hide="onDialogHide", ref="dialogRef")
   q-card.w-full
     q-card-section
       q-input(
-        :hint="domain(Bucket ?? '') === Bucket ? undefined : domain(Bucket ?? '')",
         :rules="[(v: string) => !!v || t('Item is required')]",
         clearable,
         label="bucket",
-        ref="bucket",
+        ref="bucketRef",
         v-model.trim="Bucket"
       )
         template(#prepend)
           q-icon(name="delete")
         template(#append, v-if="isElectron()")
           q-btn(:label="t('Open...')", outline, @click="getDir")
+      q-input(
+        clearable,
+        label="domain",
+        ref="domainRef",
+        v-model.trim="domain",
+        :readonly="equal",
+        :rules,
+        :hint="t('Select the checkbox if the bucket and the domain are the same')",
+      )
+        template(#prepend)
+          q-icon(name="public")
+        template(#after)
+          q-checkbox(dense, v-model="equal", :disable="isDirectory")
       q-input(clearable, label="access key id", v-model.trim="accessKeyId", hint="", :disable="isDirectory" )
         template(#prepend)
           q-icon(name="key")
@@ -67,12 +79,13 @@ q-dialog(@hide="onDialogHide", ref="dialogRef")
     q-card-actions.text-primary.bg-grey-1(align="right")
       q-btn(@click="onDialogCancel", flat, label="Cancel")
       q-btn(
-        @click="() => { bucket.validate(); if (!bucket.hasError) click(encrypt({ Bucket, secretAccessKey, region, endpoint, accessKeyId })); }",
+        @click="() => { bucketRef?.validate(); domainRef?.validate(); if (!bucketRef?.hasError && !domainRef?.hasError) click(encrypt({ Bucket, domain, secretAccessKey, region, endpoint, accessKeyId })); }",
         flat,
         label="Ok"
       )
 </template>
 <script setup lang="ts">
+import type { QInput } from "quasar";
 import type { TCredentials } from "stores/types";
 
 import { useStorage } from "@vueuse/core";
@@ -80,10 +93,9 @@ import endpoints from "assets/endpoints.json";
 import regions from "assets/regions.json";
 import CryptoJS from "crypto-js";
 import { useDialogPluginComponent, useQuasar } from "quasar";
-import { domain } from "stores/app";
 import { enumerable, mergeDefaults, writable } from "stores/defaults";
 import { validateCredentials } from "stores/types";
-import { computed, ref, triggerRef } from "vue";
+import { computed, ref, triggerRef, useTemplateRef, watch } from "vue";
 import { useI18n } from "vue-i18n";
 
 const props = defineProps<{
@@ -95,6 +107,8 @@ const $q = useQuasar();
 const { t } = useI18n();
 const { dialogRef, onDialogCancel, onDialogHide, onDialogOK } =
   useDialogPluginComponent();
+const bucketRef = useTemplateRef<QInput>("bucketRef");
+const domainRef = useTemplateRef<QInput>("domainRef");
 const creds = useStorage(
   "@",
   () => {
@@ -105,7 +119,6 @@ const creds = useStorage(
   localStorage,
   { mergeDefaults },
 );
-const bucket = ref();
 const cred = creds.value[props.value ?? ""] as
   | Record<string, null | string>
   | undefined;
@@ -114,6 +127,8 @@ const decrypt = (value?: null | string) =>
     ? CryptoJS.AES.decrypt(value ?? "", props.pin).toString(CryptoJS.enc.Utf8)
     : (value ?? null);
 const Bucket = ref(decrypt(cred?.Bucket));
+const domain = ref(decrypt(cred?.domain));
+const equal = ref(Bucket.value === domain.value);
 const secretAccessKey = ref(decrypt(cred?.secretAccessKey));
 const region = ref(decrypt(cred?.region));
 const endpoint = ref(decrypt(cred?.endpoint));
@@ -160,4 +175,24 @@ const getDir = async () => {
 const isDirectory = computed(
   () => isElectron() && window.isDirectory(Bucket.value ?? ""),
 );
+/** @see {@link https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch08s15.html} */
+const rules = [
+  (v: string) => !!v || t("Item is required"),
+  (v: string) =>
+    /\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/.test(
+      v,
+    ) || t("Not a valid domain"),
+];
+watch(isDirectory, (value) => {
+  if (value) {
+    accessKeyId.value = null;
+    secretAccessKey.value = null;
+    endpoint.value = null;
+    region.value = null;
+    equal.value = false;
+  }
+});
+watch([Bucket, equal], ([newBucket, newEqBucket]) => {
+  if (newEqBucket) domain.value = newBucket;
+});
 </script>
