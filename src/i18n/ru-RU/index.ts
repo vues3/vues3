37331c962b9d57c627c462bb86a0c5075f4d5b13
Jerry Bruwes <jbruwes@gmail.com>
2024-10-29 19:48:27 +0200

    modified:   src/i18n/ru-RU/index.ts

--- src/i18n/ru-RU/index.ts
+++ src/i18n/ru-RU/index.ts
@@ -45,4 +45,9 @@ export default Object.fromEntries([
   ["Favicon upload failed", "Сбой загрузки фавиконки"],
   ["Prohibited characters are used", "Использованы запрещенные символы"],
   ["Open...", "Открыть..."],
+  ["Not a valid domain", "Это не домен"],
+  [
+    "Select the checkbox if the bucket and the domain are the same",
+    "Поставьте галочку, если корзина и домен совпадают",
+  ],
 ]);
