3fc35e03861d8cf9f09220d9df136fc0b940a841
Jerry Bruwes <jbruwes@gmail.com>
2024-11-06 15:19:46 +0200

    modified:   src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,9 @@
 import { route } from "quasar/wrappers";
-import {
-  createMemoryHistory,
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-} from "vue-router";
+import { createMemoryHistory, createRouter } from "vue-router";
 
 import routes from "./routes";
 
 export default route(() => {
-  let createHistory;
-  if (process.env.SERVER) createHistory = createMemoryHistory;
-  else if (process.env.VUE_ROUTER_MODE === "history")
-    createHistory = createWebHistory;
-  else createHistory = createWebHashHistory;
-  return createRouter({
-    history: createHistory(process.env.VUE_ROUTER_BASE),
-    routes,
-  });
+  const history = createMemoryHistory(process.env.VUE_ROUTER_BASE);
+  return createRouter({ history, routes });
 });
