7f79e666ddb8d0712ca01d981ee677caab828982
Jerry Bruwes <jbruwes@gmail.com>
2024-11-06 13:13:23 +0200

    modified:   src/global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,31 @@
+import type { StreamingBlobPayloadInputTypes } from "@smithy/types";
 import type { Dialog } from "electron";
 
 declare global {
   interface Window {
     dialog: Dialog;
+    getObjectBlob: (
+      Bucket: string,
+      Key: string,
+      ResponseCacheControl?: string,
+    ) => Promise<Blob>;
+    getObjectText: (
+      Bucket: string,
+      Key: string,
+      ResponseCacheControl?: string,
+    ) => Promise<string>;
+    headBucket: (Bucket: string, pin?: string) => Promise<undefined>;
+    headObject: (
+      Bucket: string,
+      Key: string,
+      ResponseCacheControl?: string,
+    ) => Promise<undefined>;
     isDirectory: (path: string) => boolean;
+    putObject: (
+      Bucket: string,
+      Key: string,
+      body: StreamingBlobPayloadInputTypes,
+      ContentType: string,
+    ) => Promise<undefined>;
   }
 }
