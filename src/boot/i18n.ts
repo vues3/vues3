93e037fd1a584bc23251a2ebe992c4edeca2ef68
Jerry Bruwes <jbruwes@gmail.com>
2024-11-17 21:24:37 +0200

    modified:   src/boot/i18n.ts

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,9 +1,9 @@
+import { defineBoot } from "#q-app/wrappers";
 import { Lang } from "quasar";
-import { boot } from "quasar/wrappers";
 import messages from "src/i18n";
 import { createI18n } from "vue-i18n";
 
-export default boot(({ app }) => {
+export default defineBoot(({ app }) => {
   const locale = Lang.getLocale() === "ru-RU" ? "ru-RU" : "en-US";
   app.use(createI18n({ locale, messages }));
 });
