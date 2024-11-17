084a34b1b744c084746cf7437f5290ca947ef1ca
Jerry Bruwes <jbruwes@gmail.com>
2024-11-17 21:27:32 +0200

    modified:   src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { route } from "quasar/wrappers";
+import { defineRouter } from "#q-app/wrappers";
 import { createMemoryHistory, createRouter } from "vue-router";
 
 import routes from "./routes";
 
-export default route(() => {
+export default defineRouter(() => {
   const history = createMemoryHistory(process.env.VUE_ROUTER_BASE);
   return createRouter({ history, routes });
 });
