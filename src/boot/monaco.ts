d3ce6420828677aa91ab46e52a2b56a63818cddd
Jerry Bruwes <jbruwes@gmail.com>
2024-11-21 14:15:02 +0200

    modified:   src/boot/monaco.ts

--- src/boot/monaco.ts
+++ src/boot/monaco.ts
@@ -1,29 +1,33 @@
-import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import CssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import HtmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
-import JsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-import JsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
+import type { LanguageService } from "@vue/language-service";
+import type { Environment } from "monaco-editor-core";
 
-export default () => {
-  window.MonacoEnvironment = {
-    getWorker(workerId, label) {
-      switch (label) {
-        case "css":
-        case "less":
-        case "scss":
-          return new CssWorker();
-        case "handlebars":
-        case "html":
-        case "razor":
-          return new HtmlWorker();
-        case "javascript":
-        case "typescript":
-          return new JsWorker();
-        case "json":
-          return new JsonWorker();
-        default:
-          return new EditorWorker();
-      }
-    },
-  };
-};
+import * as volar from "@volar/monaco";
+import { editor, languages } from "monaco-editor-core";
+import EditorWorker from "monaco-editor-core/esm/vs/editor/editor.worker?worker";
+import { loadTheme } from "monaco-volar";
+import VueWorker from "monaco-volar/vue.worker?worker";
+import { loadWASM } from "onigasm";
+import onigasmWasm from "onigasm/lib/onigasm.wasm?url";
+
+languages.register({ extensions: [".vue"], id: "vue" });
+languages.onLanguage("vue", () => {
+  const getWorker: Environment["getWorker"] = (workerId, label) =>
+    label === "vue" ? new VueWorker() : new EditorWorker();
+  window.MonacoEnvironment = { getWorker };
+  const createData = {};
+  const label = "vue";
+  const moduleId = "vs/language/vue/vueWorker";
+  const worker = editor.createWebWorker<LanguageService>({
+    createData,
+    label,
+    moduleId,
+  });
+  const getSyncUris = () => editor.getModels().map(({ uri }) => uri);
+  volar.editor.activateMarkers(worker, [label], label, getSyncUris, editor);
+  volar.editor.activateAutoInsertion(worker, [label], getSyncUris, editor);
+  volar.languages
+    .registerProviders(worker, [label], getSyncUris, languages)
+    .catch(() => {});
+});
+const [theme] = await Promise.all([loadTheme(editor), loadWASM(onigasmWasm)]);
+export default theme;
