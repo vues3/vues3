6f72b988e7753689e21c21323179bd7df15e3d56
Jerry Bruwes <jbruwes@gmail.com>
2024-11-26 20:25:17 +0200

    modified:   src/boot/quasar-lang-pack.ts

--- src/boot/quasar-lang-pack.ts
+++ src/boot/quasar-lang-pack.ts
@@ -1,10 +1,7 @@
+import { defineBoot } from "#q-app/wrappers";
 import { Lang } from "quasar";
 
-export default async () => {
+export default defineBoot(async () => {
   if (Lang.getLocale() === "ru-RU")
-    Lang.set(
-      (await import("quasar/lang/ru")).default as unknown as Parameters<
-        typeof Lang.set
-      >[0],
-    );
-};
+    Lang.set((await import("quasar/lang/ru")).default);
+});
