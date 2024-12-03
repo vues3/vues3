87452abe72db5dcdaab52c314cd308aedb19787c
Jerry Bruwes <jbruwes@gmail.com>
2024-12-03 14:40:48 +0200

    modified:   monolit/src/stores/monolit.ts

--- monolit/src/stores/monolit.ts
+++ monolit/src/stores/monolit.ts
@@ -1,5 +1,5 @@
 import type { RuntimeContext } from "@unocss/runtime";
-import type { TPage } from "app/src/stores/types";
+import type { TPage } from "@vues3/types";
 import type { AsyncComponentLoader } from "vue";
 import type { AbstractPath, ContentData, File, Options } from "vue3-sfc-loader";
 import type {
