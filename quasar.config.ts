30a83a4f9c2b61b19f36c1d5e8d51183e6023af3
Jerry Bruwes <jbruwes@gmail.com>
2024-12-10 16:11:57 +0200

    modified:   quasar.config.ts

--- quasar.config.ts
+++ quasar.config.ts
@@ -1,4 +1,6 @@
+import type { QuasarBootConfiguration } from "@quasar/app-vite/types/configuration/boot";
 import type { QuasarElectronConfiguration } from "@quasar/app-vite/types/configuration/electron-conf";
+import type { QuasarFrameworkConfiguration } from "@quasar/app-vite/types/configuration/framework-conf";
 import type { SnapOptions } from "app-builder-lib";
 import type { QuasarFonts, QuasarIconSets, QuasarPlugins } from "quasar";
 
@@ -8,28 +10,32 @@ import { fileURLToPath } from "url";
 import { mergeConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 
-const boot: string[] = ["route", "quasar-lang-pack", "i18n", "monaco"];
-const css: string[] = ["app.sass"];
+/** Boot files to load. Order is important. */
+const boot: QuasarBootConfiguration = [
+  "route",
+  "quasar-lang-pack",
+  "i18n",
+  "monaco",
+];
+/**
+ * Global CSS/Stylus/SCSS/SASS/... files from /src/css/, except for theme files,
+ * which are included by default.
+ */
+const css = ["app.sass"];
+// eslint-disable-next-line tsdoc/syntax
+/** What to import from @quasar/extras package. */
 const extras: (QuasarFonts | QuasarIconSets)[] = [
   "mdi-v7",
   "roboto-font",
   "material-icons",
 ];
-const api = "modern-compiler";
-const enumerable = true;
-const sass = { api };
-const preprocessorOptions = { sass };
 const extendViteConf = (viteConf: Record<string, object>) => {
-  {
-    const value = mergeConfig(viteConf.define, {
-      __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-    });
-    Reflect.defineProperty(viteConf, "define", { value });
-  }
-  {
-    const value = mergeConfig(viteConf.css, { preprocessorOptions });
-    Reflect.defineProperty(viteConf, "css", { enumerable, value });
-  }
+  const value = mergeConfig(viteConf.define, {
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  });
+  Reflect.defineProperty(viteConf, "define", {
+    value,
+  });
 };
 const vueTsc = true;
 const lintCommand = 'eslint "./**/*.{js,ts,mjs,cjs,vue}"';
@@ -42,7 +48,7 @@ const dest = "runtime";
 const targets = [{ dest, src }];
 const vitePlugins = [
   ["@intlify/unplugin-vue-i18n/vite", { include }],
-  ["vite-plugin-checker", { eslint, vueTsc }],
+  ["vite-plugin-checker", { eslint, vueTsc }, { server: false }],
   ["@unocss/vite", { configFile, extractors }],
   [viteStaticCopy, { targets }],
 ];
@@ -53,7 +59,11 @@ const build: object = { extendViteConf, typescript, vitePlugins };
 const open = false;
 const devServer = { open };
 const plugins: (keyof QuasarPlugins)[] = ["Dialog", "Notify"];
-const framework = { plugins };
+/**
+ * What Quasar language pack to use, what Quasar icon set to use for Quasar
+ * components, etc.
+ */
+const framework: QuasarFrameworkConfiguration = { plugins };
 const bundler = "builder";
 const appId = "com.electron.vues3";
 const grade: SnapOptions["grade"] = "stable";
