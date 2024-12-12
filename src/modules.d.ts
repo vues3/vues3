df2f38460436ac14f84fdc2f6101290676e3a9b5
Jerry Bruwes <jbruwes@gmail.com>
2024-12-12 18:51:00 +0200

    new file:   src/modules.d.ts

---
+++ src/modules.d.ts
@@ -0,0 +1,3 @@
+declare module "@quasar/quasar-ui-qiconpicker/src/components/icon-set/mdi-v6";
+declare module "vue-sfc-descriptor-to-string";
+declare module "monaco-editor/esm/vs/editor/editor.worker";
