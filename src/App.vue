66cf736c73c6269849116d70f80c7d2ad4ac7308
Jerry Bruwes <jbruwes@gmail.com>
2024-12-29 19:40:50 +0200

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -66,6 +66,11 @@ q-layout(view="hHh Lpr lff")
               q-avatar(color="primary", icon="spellcheck", text-color="white")
             q-item-section
               q-item-label Fonts
+          q-item(@click="clickDomain", clickable, v-close-popup)
+            q-item-section(avatar)
+              q-avatar(color="primary", icon="public", text-color="white")
+            q-item-section
+              q-item-label Domain
           q-item(clickable, to="/", v-close-popup)
             q-item-section(avatar)
               q-avatar(color="primary", icon="logout", text-color="white")
@@ -91,14 +96,33 @@ import VFaviconDialog from "src/components/VFaviconDialog.vue";
 import VFontsDialog from "src/components/VFontsDialog.vue";
 import VImportmapDialog from "src/components/VImportmapDialog.vue";
 import VRobotsDialog from "src/components/VRobotsDialog.vue";
-import { rightDrawer } from "stores/app";
+import { domain, rightDrawer } from "stores/app";
 import { bucket } from "stores/io";
+import { useI18n } from "vue-i18n";
 // eslint-disable-next-line import/no-unresolved
 import "virtual:uno.css";
 
+const { t } = useI18n();
 const $q = useQuasar();
 const click = (component: Component) => {
   $q.dialog({ component });
+};
+const cancel = true;
+const message = t("Enter a valid domain name:");
+const persistent = true;
+const title = t("Domain");
+const isValid = (val: string) =>
+  /\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/.test(
+    val,
+  );
+const clickDomain = () => {
+  const model = domain.value;
+  const prompt = { isValid, model };
+  $q.dialog({ cancel, message, persistent, prompt, title }).onOk(
+    (data: string) => {
+      domain.value = data;
+    },
+  );
 };
 const minimize = () => {
   window.focusedWindowMinimize();
