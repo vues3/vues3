d0a2da48447839038e6e93c65d0033fec55e38a1
Jerry Bruwes <jbruwes@gmail.com>
2025-01-01 20:27:53 +0200

    modified:   src/stores/io.ts

--- src/stores/io.ts
+++ src/stores/io.ts
@@ -1,18 +1,69 @@
-import type { StreamingBlobPayloadInputTypes } from "@smithy/types";
+/* -------------------------------------------------------------------------- */
+/*                                   Imports                                  */
+/* -------------------------------------------------------------------------- */
 
+import type { HeadObjectCommandOutput } from "@aws-sdk/client-s3";
+import type { StreamingBlobPayloadInputTypes } from "@smithy/types";
+import type { Ref } from "vue";
+
+import * as fsa from "stores/fsa";
 import * as s3 from "stores/s3";
 import { ref, watch } from "vue";
 
-export const bucket = ref("");
-let fileSystemDirectoryHandle: FileSystemDirectoryHandle | undefined;
-export const setFileSystemDirectoryHandle = (
+/* -------------------------------------------------------------------------- */
+/*                                 References                                 */
+/* -------------------------------------------------------------------------- */
+
+/** Bucket name */
+
+const bucket: Ref<string> = ref("");
+
+/* -------------------------------------------------------------------------- */
+
+/** Remote flag */
+
+const remote: Ref<boolean> = ref(false);
+
+/* -------------------------------------------------------------------------- */
+/*                                  Variables                                 */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * The FileSystemDirectoryHandle interface of the File System API provides a
+ * handle to a file system directory.
+ */
+
+let fileSystemDirectoryHandle: FileSystemDirectoryHandle | null = null;
+
+/* -------------------------------------------------------------------------- */
+/*                                  Functions                                 */
+/* -------------------------------------------------------------------------- */
+
+/** FileSystemDirectoryHandle setter */
+
+const setFileSystemDirectoryHandle: (
   value: FileSystemDirectoryHandle,
-) => {
+) => void = (value) => {
   fileSystemDirectoryHandle = value;
 };
-const remote = ref(false);
-const io = () => (remote.value ? s3 : window);
-export const headBucket = async (Bucket: string, pin?: string) => {
+
+/* -------------------------------------------------------------------------- */
+
+/** IO system selector */
+
+const io: () => typeof s3 | Window = () => (remote.value ? s3 : window);
+
+/* -------------------------------------------------------------------------- */
+
+/**
+ * You can use this operation to determine if a bucket exists and if you have
+ * permission to access it.
+ */
+
+const headBucket: (Bucket: string, pin?: string) => Promise<void> = async (
+  Bucket,
+  pin,
+) => {
   try {
     await s3.headBucket(Bucket, pin);
     remote.value = true;
@@ -21,33 +72,122 @@ export const headBucket = async (Bucket: string, pin?: string) => {
     throw new Error(message);
   }
 };
-export const headObject = async (Key: string, ResponseCacheControl?: string) =>
-  io().headObject(bucket.value, Key, ResponseCacheControl);
-export const putObject = async (
+
+/* -------------------------------------------------------------------------- */
+
+/**
+ * The HEAD operation retrieves metadata from an object without returning the
+ * object itself. This operation is useful if you're interested only in an
+ * object's metadata.
+ */
+
+const headObject: (
+  Key: string,
+  ResponseCacheControl?: string,
+) => Promise<HeadObjectCommandOutput | null> = async (
+  Key,
+  ResponseCacheControl,
+) => {
+  if (fileSystemDirectoryHandle)
+    return fsa.headObject(fileSystemDirectoryHandle, Key);
+  return io().headObject(bucket.value, Key, ResponseCacheControl);
+};
+
+/* -------------------------------------------------------------------------- */
+
+/** Adds an object to a bucket */
+
+const putObject: (
   Key: string,
   body: StreamingBlobPayloadInputTypes,
   ContentType: string,
-) => {
-  if (bucket.value) await io().putObject(bucket.value, Key, body, ContentType);
+) => Promise<void> = async (Key, body, ContentType) => {
+  if (bucket.value)
+    if (fileSystemDirectoryHandle)
+      await fsa.putObject(fileSystemDirectoryHandle, Key, body);
+    else await io().putObject(bucket.value, Key, body, ContentType);
 };
-export const removeEmptyDirectories = async () => {
-  if (bucket.value) await io().removeEmptyDirectories?.(bucket.value);
+
+/* -------------------------------------------------------------------------- */
+
+/** Remove empty directories */
+
+const removeEmptyDirectories: () => Promise<void> = async () => {
+  if (bucket.value)
+    if (fileSystemDirectoryHandle) {
+      // console.log("removeEmptyDirectories");
+    } else await io().removeEmptyDirectories?.(bucket.value);
 };
-export const deleteObject = async (Key: string) => {
-  if (bucket.value) await io().deleteObject(bucket.value, Key);
+
+/* -------------------------------------------------------------------------- */
+
+/** Removes an object from a bucket */
+
+const deleteObject: (Key: string) => Promise<void> = async (Key) => {
+  if (bucket.value)
+    if (fileSystemDirectoryHandle)
+      await fsa.deleteObject(fileSystemDirectoryHandle, Key);
+    else await io().deleteObject(bucket.value, Key);
 };
-export const getObjectText = async (
+
+/* -------------------------------------------------------------------------- */
+
+/** Retrieves a text object */
+
+const getObjectText: (
   Key: string,
   ResponseCacheControl?: string,
-) => io().getObjectText(bucket.value, Key, ResponseCacheControl);
-export const getObjectBlob = async (
+) => Promise<string> = async (Key, ResponseCacheControl) => {
+  if (fileSystemDirectoryHandle)
+    return fsa.getObjectText(fileSystemDirectoryHandle, Key);
+  return io().getObjectText(bucket.value, Key, ResponseCacheControl);
+};
+
+/* -------------------------------------------------------------------------- */
+
+/** Retrieves a blob object */
+
+const getObjectBlob: (
   Key: string,
   ResponseCacheControl?: string,
-) => io().getObjectBlob(bucket.value, Key, ResponseCacheControl);
-watch(bucket, (value) => {
+) => Promise<Blob> = async (Key, ResponseCacheControl) => {
+  if (fileSystemDirectoryHandle)
+    return fsa.getObjectBlob(fileSystemDirectoryHandle, Key);
+  return io().getObjectBlob(bucket.value, Key, ResponseCacheControl);
+};
+
+/* -------------------------------------------------------------------------- */
+
+/** A callback function for the bucket watcher */
+
+const cbBucket: (value: string) => void = (value) => {
   if (!value) {
-    s3.setS3Client(undefined);
+    s3.setS3Client(null);
     remote.value = false;
-    if (fileSystemDirectoryHandle) fileSystemDirectoryHandle = undefined;
+    if (fileSystemDirectoryHandle) fileSystemDirectoryHandle = null;
   }
-});
+};
+
+/* -------------------------------------------------------------------------- */
+/*                                    Main                                    */
+/* -------------------------------------------------------------------------- */
+
+watch(bucket, cbBucket);
+
+/* -------------------------------------------------------------------------- */
+/*                                   Exports                                  */
+/* -------------------------------------------------------------------------- */
+
+export {
+  bucket,
+  deleteObject,
+  getObjectBlob,
+  getObjectText,
+  headBucket,
+  headObject,
+  putObject,
+  removeEmptyDirectories,
+  setFileSystemDirectoryHandle,
+};
+
+/* -------------------------------------------------------------------------- */
