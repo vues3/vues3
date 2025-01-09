d09a0097e10a8694ba5a9e02132730fea001258b
Jerry Bruwes <jbruwes@gmail.com>
2025-01-09 14:26:17 +0200

    modified:   src/boot/i18n.ts

--- src/boot/i18n.ts
+++ src/boot/i18n.ts
@@ -1,9 +1,27 @@
+/* -------------------------------------------------------------------------- */
+/*                                   Imports                                  */
+/* -------------------------------------------------------------------------- */
+
+import type { BootCallback } from "@quasar/app-vite";
+
 import { defineBoot } from "#q-app/wrappers";
 import { Lang } from "quasar";
 import messages from "src/i18n";
 import { createI18n } from "vue-i18n";
 
-export default defineBoot(({ app }) => {
+/* -------------------------------------------------------------------------- */
+/*                                  Functions                                 */
+/* -------------------------------------------------------------------------- */
+
+const callback: BootCallback = ({ app }) => {
   const locale = Lang.getLocale() === "ru-RU" ? "ru-RU" : "en-US";
   app.use(createI18n({ locale, messages }));
-});
+};
+
+/* -------------------------------------------------------------------------- */
+/*                                   Exports                                  */
+/* -------------------------------------------------------------------------- */
+
+export default defineBoot(callback);
+
+/* -------------------------------------------------------------------------- */
