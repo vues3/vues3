11f2d8d0d10f46134b30a5c7aa39d267e5a5458e
Jerry Bruwes <jbruwes@gmail.com>
2025-01-15 13:03:09 +0200

    modified:   src-electron/electron-preload.ts

--- src-electron/electron-preload.ts
+++ src-electron/electron-preload.ts
@@ -31,24 +31,90 @@ const recursive = true;
 
 /** Check the file existence */
 
-const headObject: (Bucket: string, Key: string) => Promise<null> = async (
-  Bucket,
-  Key,
-) => {
-  const stats = await lstat(join(Bucket, Key));
-  if (stats.isFile()) return null;
-  throw new Error("It's not a file");
-};
+async function deleteObject(Bucket: string, Key: string): Promise<void> {
+  await unlink(join(Bucket, Key));
+}
 
 /* -------------------------------------------------------------------------- */
 
 /** Adds an object to a bucket */
 
-const putObject: (
+function focusedWindowClose(): void {
+  BrowserWindow.getFocusedWindow()?.close();
+}
+
+/* -------------------------------------------------------------------------- */
+
+/** Remove empty directories */
+
+function focusedWindowIsMaximized(): boolean | null {
+  return BrowserWindow.getFocusedWindow()?.isMaximized() ?? null;
+}
+
+/* -------------------------------------------------------------------------- */
+
+/** Removes an object from a bucket */
+
+function focusedWindowMinimize(): void {
+  BrowserWindow.getFocusedWindow()?.minimize();
+}
+
+/* -------------------------------------------------------------------------- */
+
+/** Retrieves an object */
+
+function focusedWindowToggleMaximize(): void {
+  const focusedWindow = BrowserWindow.getFocusedWindow();
+  if (focusedWindow?.isMaximized()) focusedWindow.unmaximize();
+  else focusedWindow?.maximize();
+}
+
+/* -------------------------------------------------------------------------- */
+
+/** Retrieves a text object */
+
+async function getObject(Bucket: string, Key: string): Promise<Response> {
+  try {
+    const file = join(Bucket, Key);
+    const [body, mime] = await Promise.all([readFile(file), import("mime")]);
+    const type = mime.default.getType(file);
+    const headers = new Headers(type ? { "content-type": type } : undefined);
+    return new Response(body, { headers });
+  } catch {
+    //
+  }
+  return new Response();
+}
+
+/* -------------------------------------------------------------------------- */
+
+/** Retrieves a blob object */
+
+async function getObjectBlob(Bucket: string, Key: string): Promise<Blob> {
+  return (await getObject(Bucket, Key)).blob();
+}
+
+/* -------------------------------------------------------------------------- */
+
+async function getObjectText(Bucket: string, Key: string): Promise<string> {
+  return (await getObject(Bucket, Key)).text();
+}
+
+/* -------------------------------------------------------------------------- */
+
+async function headObject(Bucket: string, Key: string): Promise<null> {
+  const stats = await lstat(join(Bucket, Key));
+  if (stats.isFile()) return null;
+  throw new Error("It's not a file");
+}
+
+/* -------------------------------------------------------------------------- */
+
+async function putObject(
   Bucket: string,
   Key: string,
   body: StreamingBlobPayloadInputTypes,
-) => Promise<void> = async (Bucket, Key, body) => {
+): Promise<void> {
   const filePath = join(Bucket, Key);
   const dirName = dirname(filePath);
   try {
@@ -57,16 +123,14 @@ const putObject: (
     await mkdir(dirName, { recursive });
   }
   await writeFile(filePath, body as string | Uint8Array);
-};
+}
 
 /* -------------------------------------------------------------------------- */
 
-/** Remove empty directories */
-
-const removeEmptyDirectories: (
+async function removeEmptyDirectories(
   directory: string,
   exclude: string[],
-) => Promise<void> = async (directory, exclude) => {
+): Promise<void> {
   const fileStats = await lstat(directory);
   if (!fileStats.isDirectory() || exclude.includes(basename(directory))) return;
   let fileNames = await readdir(directory);
@@ -79,81 +143,7 @@ const removeEmptyDirectories: (
     fileNames = await readdir(directory);
   }
   if (!fileNames.length) await rmdir(directory);
-};
-
-/* -------------------------------------------------------------------------- */
-
-/** Removes an object from a bucket */
-
-const deleteObject: (Bucket: string, Key: string) => Promise<void> = async (
-  Bucket,
-  Key,
-) => {
-  await unlink(join(Bucket, Key));
-};
-
-/* -------------------------------------------------------------------------- */
-
-/** Retrieves an object */
-
-const getObject: (Bucket: string, Key: string) => Promise<Response> = async (
-  Bucket,
-  Key,
-) => {
-  try {
-    const file = join(Bucket, Key);
-    const [body, mime] = await Promise.all([readFile(file), import("mime")]);
-    const type = mime.default.getType(file);
-    const headers = new Headers(type ? { "content-type": type } : undefined);
-    return new Response(body, { headers });
-  } catch {
-    //
-  }
-  return new Response();
-};
-
-/* -------------------------------------------------------------------------- */
-
-/** Retrieves a text object */
-
-const getObjectText: (Bucket: string, Key: string) => Promise<string> = async (
-  Bucket,
-  Key,
-) => (await getObject(Bucket, Key)).text();
-
-/* -------------------------------------------------------------------------- */
-
-/** Retrieves a blob object */
-
-const getObjectBlob: (Bucket: string, Key: string) => Promise<Blob> = async (
-  Bucket,
-  Key,
-) => (await getObject(Bucket, Key)).blob();
-
-/* -------------------------------------------------------------------------- */
-
-const focusedWindowClose: () => void = () => {
-  BrowserWindow.getFocusedWindow()?.close();
-};
-
-/* -------------------------------------------------------------------------- */
-
-const focusedWindowMinimize: () => void = () => {
-  BrowserWindow.getFocusedWindow()?.minimize();
-};
-
-/* -------------------------------------------------------------------------- */
-
-const focusedWindowToggleMaximize: () => void = () => {
-  const focusedWindow = BrowserWindow.getFocusedWindow();
-  if (focusedWindow?.isMaximized()) focusedWindow.unmaximize();
-  else focusedWindow?.maximize();
-};
-
-/* -------------------------------------------------------------------------- */
-
-const focusedWindowIsMaximized: () => boolean | null = () =>
-  BrowserWindow.getFocusedWindow()?.isMaximized() ?? null;
+}
 
 /* -------------------------------------------------------------------------- */
 /*                                    Main                                    */
