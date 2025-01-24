f1ff1acba928ee80017df2a1d94a52cf08fa36ef
Jerry Bruwes <jbruwes@gmail.com>
2025-01-24 18:01:31 +0200

    modified:   src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,19 @@
+/* -------------------------------------------------------------------------- */
+/*                                   Imports                                  */
+/* -------------------------------------------------------------------------- */
+
 import { defineRouter } from "#q-app/wrappers";
 import { createMemoryHistory, createRouter } from "vue-router";
 
 import routes from "./routes";
 
+/* -------------------------------------------------------------------------- */
+/*                                   Exports                                  */
+/* -------------------------------------------------------------------------- */
+
 export default defineRouter(() => {
   const history = createMemoryHistory(process.env.VUE_ROUTER_BASE);
   return createRouter({ history, routes });
 });
+
+/* -------------------------------------------------------------------------- */
