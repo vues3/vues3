4783cbd1b5f5187daf2da07310fe754741feda26
Jerry Bruwes <jbruwes@gmail.com>
2025-01-28 17:42:39 +0200

    modified:   uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -1,17 +1,22 @@
 import type { Preset } from "@unocss/core";
 
-import presetAttributify from "@unocss/preset-attributify";
-import presetIcons from "@unocss/preset-icons/browser";
-import presetTagify from "@unocss/preset-tagify";
-import presetTypography from "@unocss/preset-typography";
-import presetUno from "@unocss/preset-uno";
+import attributify from "@unocss/preset-attributify";
+import icons from "@unocss/preset-icons/browser";
+import tagify from "@unocss/preset-tagify";
+import typography from "@unocss/preset-typography";
+import uno from "@unocss/preset-uno";
 
-const cdn = "https://cdn.jsdelivr.net/npm/";
-const presets: Preset[] = [
-  presetUno(),
-  presetTypography(),
-  presetIcons({ cdn }),
-  presetTagify(),
-  presetAttributify(),
-];
+/* -------------------------------------------------------------------------- */
+
+const cdn = "https://cdn.jsdelivr.net/npm/",
+  presets: Preset[] = [
+    uno(),
+    typography(),
+    icons({ cdn }),
+    tagify(),
+    attributify(),
+  ];
+
+/* -------------------------------------------------------------------------- */
+
 export default { presets };
