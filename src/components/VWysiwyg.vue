a1d91c06cfe633206b3ec236ed7772ab20278f84
Jerry Bruwes <jbruwes@gmail.com>
2025-01-30 18:35:26 +0200

    modified:   src/components/VWysiwyg.vue

--- src/components/VWysiwyg.vue
+++ src/components/VWysiwyg.vue
@@ -23,22 +23,14 @@ form(
 </template>
 
 <script setup lang="ts">
-/* -------------------------------------------------------------------------- */
-/*                                   Imports                                  */
-/* -------------------------------------------------------------------------- */
-
-import type { RuntimeOptions } from "@unocss/runtime";
-import type { UseFileDialogReturn } from "@vueuse/core";
 import type {
   QEditor,
   QEditorProps,
   QuasarIconSetEditor,
   QuasarLanguageEditorLabel,
-  QVueGlobals,
   StringDictionary,
 } from "quasar";
 import type { Component, Ref } from "vue";
-import type { Composer } from "vue-i18n";
 
 import webFonts from "@unocss/preset-web-fonts";
 import initUnocssRuntime from "@unocss/runtime";
@@ -56,59 +48,31 @@ import { computed, nextTick, onMounted, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 
 /* -------------------------------------------------------------------------- */
-/*                                 Properties                                 */
-/* -------------------------------------------------------------------------- */
 
-const props = withDefaults(
-  defineProps<{
-    id: string | undefined;
-    modelValue: Promise<string> | string;
-  }>(),
-  {
-    id: "",
-    modelValue: "",
-  },
-);
-
-/* -------------------------------------------------------------------------- */
-/*                                   Objects                                  */
-/* -------------------------------------------------------------------------- */
-
-const $q: QVueGlobals = useQuasar();
+const { t } = useI18n();
 
 /* -------------------------------------------------------------------------- */
 
-const { t }: Composer = useI18n();
-
-/* -------------------------------------------------------------------------- */
-/*                                  Constants                                 */
-/* -------------------------------------------------------------------------- */
-
-const placeholder = t("Add some content to your page...");
-
-/* -------------------------------------------------------------------------- */
-
-const message = t("The graphic file type is not suitable for use on the web");
-
-/* -------------------------------------------------------------------------- */
-
-const list = "no-icons";
-
-/* -------------------------------------------------------------------------- */
-/*                                 References                                 */
-/* -------------------------------------------------------------------------- */
-
-const editor: Ref<QEditor | undefined> = ref();
+const $q = useQuasar(),
+  editor: Ref<QEditor | undefined> = ref(),
+  props = withDefaults(
+    defineProps<{
+      id: string | undefined;
+      modelValue: Promise<string> | string;
+    }>(),
+    {
+      id: "",
+      modelValue: "",
+    },
+  ),
+  htm = ref(await props.modelValue),
+  list = "no-icons",
+  message = t("The graphic file type is not suitable for use on the web"),
+  placeholder = t("Add some content to your page...");
 
 /* -------------------------------------------------------------------------- */
 
-const htm: Ref<string> = ref(await props.modelValue);
-
-/* -------------------------------------------------------------------------- */
-/*                                  Functions                                 */
-/* -------------------------------------------------------------------------- */
-
-const insertImage = (file: File): void => {
+const insertImage = (file: File) => {
   const { type } = file;
   if (mimes.includes(type)) {
     const filePath = `images/${uid()}.${mime.getExtension(type) ?? ""}`;
@@ -122,10 +86,7 @@ const insertImage = (file: File): void => {
     );
   } else $q.notify({ message });
 };
-
-/* -------------------------------------------------------------------------- */
-
-const capture = (evt: ClipboardEvent | DragEvent): void => {
+const capture = (evt: ClipboardEvent | DragEvent) => {
   const { files = [] } =
     (evt as DragEvent).dataTransfer ??
     (evt as ClipboardEvent).clipboardData ??
@@ -138,14 +99,12 @@ const capture = (evt: ClipboardEvent | DragEvent): void => {
 };
 
 /* -------------------------------------------------------------------------- */
-/*                                   Objects                                  */
-/* -------------------------------------------------------------------------- */
 
-const { files, open }: UseFileDialogReturn = useFileDialog({ accept, reset });
+const { files, open } = useFileDialog({ accept, reset });
 
 /* -------------------------------------------------------------------------- */
 
-const definitions: QEditorProps["definitions"] = {
+const definitions = {
   ...(Object.fromEntries(
     [
       ["upload", t("Upload Image"), open],
@@ -161,7 +120,7 @@ const definitions: QEditorProps["definitions"] = {
       ],
     ].map(([icon, tip, handler]) => [icon, { handler, icon, tip }]),
   ) as QEditorProps["definitions"]),
-  ...(Object.fromEntries(
+  ...Object.fromEntries(
     [
       ...[...Array(6).keys()].map((key) => [
         `h${String(key + 1)}`,
@@ -175,51 +134,31 @@ const definitions: QEditorProps["definitions"] = {
         htmlTip: `<span class="prose"><${key} class="!my-0">${$q.lang.editor[value as keyof StringDictionary<QuasarLanguageEditorLabel>]}</${key}></span>`,
       },
     ]),
-  ) as QEditorProps["definitions"]),
+  ),
 };
 
 /* -------------------------------------------------------------------------- */
-/*                                  Watchers                                  */
-/* -------------------------------------------------------------------------- */
+
+defineEmits(["update:modelValue"]);
 
 watch(files, (newFiles) => {
   if (newFiles) [...newFiles].forEach(insertImage);
 });
 
-/* -------------------------------------------------------------------------- */
-/*                                    Main                                    */
-/* -------------------------------------------------------------------------- */
-
-defineEmits(["update:modelValue"]);
-
-/* -------------------------------------------------------------------------- */
-
 onMounted(() => {
-  /* -------------------------------------------------------------------------- */
-  /*                                   Objects                                  */
-  /* -------------------------------------------------------------------------- */
-
-  const rootElement: RuntimeOptions["rootElement"] = editor.value?.getContentEl;
-
-  /* -------------------------------------------------------------------------- */
-  /*                                  Watchers                                  */
-  /* -------------------------------------------------------------------------- */
-
+  const rootElement = editor.value?.getContentEl;
   watch(
     () => getFonts(Fonts),
     async (fonts) => {
       if (rootElement) {
         let { presets } = Defaults;
         presets = [...presets, webFonts({ customFetch, fonts })];
-        const defaults: RuntimeOptions["defaults"] = { presets };
+        const defaults = { presets };
         await initUnocssRuntime({ bypassDefined, defaults, rootElement });
       }
     },
     { immediate },
   );
-
-  /* -------------------------------------------------------------------------- */
-
   watch(
     () => props.id,
     async () => {
@@ -232,78 +171,69 @@ onMounted(() => {
         contentEl.innerHTML = htm.value;
     },
   );
-
-  /* -------------------------------------------------------------------------- */
 });
 
 /* -------------------------------------------------------------------------- */
-/*                                Computations                                */
-/* -------------------------------------------------------------------------- */
 
 const fonts = computed(() => ({
-  ...getFonts([
-    "Arial",
-    "Arial Black",
-    "Comic Sans",
-    "Courier New",
-    "Impact",
-    "Lucida Grande",
-    "Times New Roman",
-    "Verdana",
-  ]),
-  ...getFonts(Fonts),
-}));
-
-/* -------------------------------------------------------------------------- */
-
-const toolbar = computed(() => [
-  ["left", "center", "right", "justify"],
-  ["bold", "italic", "strike", "underline", "subscript", "superscript"],
-  ["hr", "link"],
-  ["print", "fullscreen"],
-  [
-    ...[
-      [
-        "formatting",
+    ...getFonts([
+      "Arial",
+      "Arial Black",
+      "Comic Sans",
+      "Courier New",
+      "Impact",
+      "Lucida Grande",
+      "Times New Roman",
+      "Verdana",
+    ]),
+    ...getFonts(Fonts),
+  })),
+  toolbar = computed(() => [
+    ["left", "center", "right", "justify"],
+    ["bold", "italic", "strike", "underline", "subscript", "superscript"],
+    ["hr", "link"],
+    ["print", "fullscreen"],
+    [
+      ...[
         [
-          "p",
-          ...[...Array(6).keys()].map((key) => `h${String(key + 1)}`),
-          "code",
+          "formatting",
+          [
+            "p",
+            ...[...Array(6).keys()].map((key) => `h${String(key + 1)}`),
+            "code",
+          ],
+          false,
+          false,
         ],
-        false,
-        false,
-      ],
-      [
-        "fontSize",
-        [...Array(7).keys()].map((key) => `size-${String(key + 1)}`),
-        true,
-        true,
-      ],
-      [
-        "defaultFont",
-        ["default_font", ...Object.keys(fonts.value)],
-        false,
-        true,
-      ],
-    ].map(([key, options, fixedLabel, fixedIcon]) => ({
-      fixedIcon,
-      fixedLabel,
-      icon: $q.iconSet.editor[
-        key as keyof StringDictionary<QuasarIconSetEditor>
-      ],
-      label:
-        $q.lang.editor[
-          key as keyof StringDictionary<QuasarLanguageEditorLabel>
+        [
+          "fontSize",
+          [...Array(7).keys()].map((key) => `size-${String(key + 1)}`),
+          true,
+          true,
         ],
-      list,
-      options,
-    })),
-    "removeFormat",
-  ],
-  ["quote", "unordered", "ordered", "outdent", "indent"],
-  ["undo", "redo"],
-  ["upload", "dashboard", "share"],
-]);
-
-/* -------------------------------------------------------------------------- */
+        [
+          "defaultFont",
+          ["default_font", ...Object.keys(fonts.value)],
+          false,
+          true,
+        ],
+      ].map(([key, options, fixedLabel, fixedIcon]) => ({
+        fixedIcon,
+        fixedLabel,
+        icon: $q.iconSet.editor[
+          key as keyof StringDictionary<QuasarIconSetEditor>
+        ],
+        label:
+          $q.lang.editor[
+            key as keyof StringDictionary<QuasarLanguageEditorLabel>
+          ],
+        list,
+        options,
+      })),
+      "removeFormat",
+    ],
+    ["quote", "unordered", "ordered", "outdent", "indent"],
+    ["undo", "redo"],
+    ["upload", "dashboard", "share"],
+  ]);
 </script>
