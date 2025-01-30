88e5eb74c09494ddf2b4d408633307d55ced71d1
Jerry Bruwes <jbruwes@gmail.com>
2025-01-30 18:39:46 +0200

    modified:   src-electron/electron-preload.ts

--- src-electron/electron-preload.ts
+++ src-electron/electron-preload.ts
@@ -1,6 +1,3 @@
-/* -------------------------------------------------------------------------- */
-/*                                   Imports                                  */
-/* -------------------------------------------------------------------------- */
 import type { StreamingBlobPayloadInputTypes } from "@smithy/types";
 
 import { BrowserWindow, dialog } from "@electron/remote";
@@ -18,132 +15,78 @@ import {
 import { basename, dirname, join } from "path";
 
 /* -------------------------------------------------------------------------- */
-/*                                  Constants                                 */
-/* -------------------------------------------------------------------------- */
-
-/** A recursive property indicating whether parent directories should be created */
 
 const recursive = true;
 
 /* -------------------------------------------------------------------------- */
-/*                                  Functions                                 */
-/* -------------------------------------------------------------------------- */
 
-/** Check the file existence */
+const deleteObject = async (Bucket: string, Key: string) => {
+    await unlink(join(Bucket, Key));
+  },
+  focusedWindowClose = () => {
+    BrowserWindow.getFocusedWindow()?.close();
+  },
+  focusedWindowIsMaximized = () =>
+    BrowserWindow.getFocusedWindow()?.isMaximized(),
+  focusedWindowMinimize = () => {
+    BrowserWindow.getFocusedWindow()?.minimize();
+  },
+  focusedWindowToggleMaximize = () => {
+    const focusedWindow = BrowserWindow.getFocusedWindow();
+    if (focusedWindow?.isMaximized()) focusedWindow.unmaximize();
+    else focusedWindow?.maximize();
+  },
+  getObject = async (Bucket: string, Key: string) => {
+    try {
+      const file = join(Bucket, Key);
+      const [body, mime] = await Promise.all([readFile(file), import("mime")]);
+      const type = mime.default.getType(file);
+      const headers = new Headers(type ? { "content-type": type } : undefined);
+      return new Response(body, { headers });
+    } catch {
+      //
+    }
+    return new Response();
+  },
+  getObjectBlob = async (Bucket: string, Key: string) =>
+    (await getObject(Bucket, Key)).blob(),
+  getObjectText = async (Bucket: string, Key: string) =>
+    (await getObject(Bucket, Key)).text(),
+  headObject = async (Bucket: string, Key: string) => {
+    const stats = await lstat(join(Bucket, Key));
+    if (stats.isFile()) return undefined;
+    throw new Error("It's not a file");
+  },
+  putObject = async (
+    Bucket: string,
+    Key: string,
+    body: StreamingBlobPayloadInputTypes,
+  ) => {
+    const filePath = join(Bucket, Key);
+    const dirName = dirname(filePath);
+    try {
+      await access(dirName);
+    } catch {
+      await mkdir(dirName, { recursive });
+    }
+    await writeFile(filePath, body as string | Uint8Array);
+  },
+  removeEmptyDirectories = async (directory: string, exclude: string[]) => {
+    const fileStats = await lstat(directory);
+    if (!fileStats.isDirectory() || exclude.includes(basename(directory)))
+      return;
+    let fileNames = await readdir(directory);
+    if (fileNames.length) {
+      await Promise.all(
+        fileNames.map((fileName) =>
+          removeEmptyDirectories(join(directory, fileName), exclude),
+        ),
+      );
+      fileNames = await readdir(directory);
+    }
+    if (!fileNames.length) await rmdir(directory);
+  };
 
-const deleteObject = async (Bucket: string, Key: string): Promise<void> => {
-  await unlink(join(Bucket, Key));
-};
-
-/* -------------------------------------------------------------------------- */
-
-/** Adds an object to a bucket */
-
-const focusedWindowClose = (): void => {
-  BrowserWindow.getFocusedWindow()?.close();
-};
-
-/* -------------------------------------------------------------------------- */
-
-/** Remove empty directories */
-
-const focusedWindowIsMaximized = (): boolean | undefined =>
-  BrowserWindow.getFocusedWindow()?.isMaximized();
-
-/* -------------------------------------------------------------------------- */
-
-/** Removes an object from a bucket */
-
-const focusedWindowMinimize = (): void => {
-  BrowserWindow.getFocusedWindow()?.minimize();
-};
-
-/* -------------------------------------------------------------------------- */
-
-/** Retrieves an object */
-
-const focusedWindowToggleMaximize = (): void => {
-  const focusedWindow = BrowserWindow.getFocusedWindow();
-  if (focusedWindow?.isMaximized()) focusedWindow.unmaximize();
-  else focusedWindow?.maximize();
-};
-
-/* -------------------------------------------------------------------------- */
-
-/** Retrieves a text object */
-
-const getObject = async (Bucket: string, Key: string): Promise<Response> => {
-  try {
-    const file = join(Bucket, Key);
-    const [body, mime] = await Promise.all([readFile(file), import("mime")]);
-    const type = mime.default.getType(file);
-    const headers = new Headers(type ? { "content-type": type } : undefined);
-    return new Response(body, { headers });
-  } catch {
-    //
-  }
-  return new Response();
-};
-
-/* -------------------------------------------------------------------------- */
-
-/** Retrieves a blob object */
-
-const getObjectBlob = async (Bucket: string, Key: string): Promise<Blob> =>
-  (await getObject(Bucket, Key)).blob();
-
-/* -------------------------------------------------------------------------- */
-
-const getObjectText = async (Bucket: string, Key: string): Promise<string> =>
-  (await getObject(Bucket, Key)).text();
-
-/* -------------------------------------------------------------------------- */
-
-const headObject = async (Bucket: string, Key: string): Promise<undefined> => {
-  const stats = await lstat(join(Bucket, Key));
-  if (stats.isFile()) return undefined;
-  throw new Error("It's not a file");
-};
-
-/* -------------------------------------------------------------------------- */
-
-const putObject = async (
-  Bucket: string,
-  Key: string,
-  body: StreamingBlobPayloadInputTypes,
-): Promise<void> => {
-  const filePath = join(Bucket, Key);
-  const dirName = dirname(filePath);
-  try {
-    await access(dirName);
-  } catch {
-    await mkdir(dirName, { recursive });
-  }
-  await writeFile(filePath, body as string | Uint8Array);
-};
-
-/* -------------------------------------------------------------------------- */
-
-const removeEmptyDirectories = async (
-  directory: string,
-  exclude: string[],
-): Promise<void> => {
-  const fileStats = await lstat(directory);
-  if (!fileStats.isDirectory() || exclude.includes(basename(directory))) return;
-  let fileNames = await readdir(directory);
-  if (fileNames.length) {
-    await Promise.all(
-      fileNames.map((fileName) =>
-        removeEmptyDirectories(join(directory, fileName), exclude),
-      ),
-    );
-    fileNames = await readdir(directory);
-  }
-  if (!fileNames.length) await rmdir(directory);
-};
-
-/* -------------------------------------------------------------------------- */
-/*                                    Main                                    */
 /* -------------------------------------------------------------------------- */
 
 Object.entries({
@@ -161,5 +104,3 @@ Object.entries({
 }).forEach(([apiKey, api]) => {
   contextBridge.exposeInMainWorld(apiKey, api);
 });
-
-/* -------------------------------------------------------------------------- */
