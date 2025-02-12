5385d8e0435ea5a0db29b9ead5092219d2533054
Jerry Bruwes <jbruwes@icloud.com>
2025-02-12 16:02:29 +0200

    modified:   src/components/VWysiwyg.vue

--- src/components/VWysiwyg.vue
+++ src/components/VWysiwyg.vue
@@ -1,24 +1,32 @@
+<!-- eslint-disable vue/no-v-text-v-html-on-component -->
 <template lang="pug">
-form(
+form.col.column(
   autocapitalize="on",
   autocomplete="on",
   autocorrect="on",
   spellcheck="true"
 )
-  q-editor.col.column.full-width(
+  q-editor.col.column(
     :definitions,
     :dense="$q.screen.lt.md",
     :fonts,
     :model-value="htm",
     :placeholder,
     :toolbar,
-    @drop="capture",
-    @paste="capture",
+    @drop="paste",
+    @paste="paste",
     @update:model-value="$emit('update:modelValue', $event)",
-    @vue:mounted="nextTick(editor?.focus)",
     content-class="col prose max-w-none",
     flat,
     ref="editor"
+  )
+  q-tooltip.text-caption.bg-primary(
+    :target,
+    anchor="top left",
+    self="top left",
+    :scroll-target,
+    v-model="show",
+    v-html="tooltip"
   )
 </template>
 
@@ -44,17 +52,46 @@ import { uid, useQuasar } from "quasar";
 import { fonts as Fonts, urls } from "stores/app";
 import { accept, bypassDefined, immediate, reset } from "stores/defaults";
 import { putObject } from "stores/io";
-import { computed, nextTick, onMounted, ref, watch } from "vue";
+import { computed, nextTick, onMounted, onUnmounted, ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
+
+/* -------------------------------------------------------------------------- */
+
+let blocks = false,
+  rootElement: () => Element | undefined;
+
+/* -------------------------------------------------------------------------- */
+
+watch(
+  () => getFonts(Fonts),
+  async (fonts) => {
+    let { presets } = Defaults;
+    presets = [...presets, webFonts({ customFetch, fonts })];
+    const defaults = { presets };
+    await initUnocssRuntime({ bypassDefined, defaults, rootElement });
+  },
+  { immediate },
+);
 
 /* -------------------------------------------------------------------------- */
 
 const { t } = useI18n();
 
-/* -------------------------------------------------------------------------- */
-
 const $q = useQuasar(),
   editor: Ref<QEditor | undefined> = ref(),
+  fonts = computed(() => ({
+    ...getFonts([
+      "Arial",
+      "Arial Black",
+      "Comic Sans",
+      "Courier New",
+      "Impact",
+      "Lucida Grande",
+      "Times New Roman",
+      "Verdana",
+    ]),
+    ...getFonts(Fonts),
+  })),
   props = withDefaults(
     defineProps<{
       id: string | undefined;
@@ -68,131 +105,15 @@ const $q = useQuasar(),
   htm = ref(await props.modelValue),
   list = "no-icons",
   message = t("The graphic file type is not suitable for use on the web"),
-  placeholder = t("Add some content to your page...");
-
-/* -------------------------------------------------------------------------- */
-
-const insertImage = (file: File) => {
-  const { type } = file;
-  if (mimes.includes(type)) {
-    const filePath = `images/${uid()}.${mime.getExtension(type) ?? ""}`;
-    (async () => {
-      await putObject(filePath, new Uint8Array(await file.arrayBuffer()), type);
-    })().catch(consoleError);
-    urls.set(filePath, URL.createObjectURL(file));
-    editor.value?.runCmd(
-      "insertHTML",
-      `<img src="${urls.get(filePath) ?? ""}" data-src="${filePath}" alt="" decoding="async" loading="lazy" />`,
-    );
-  } else $q.notify({ message });
-};
-const capture = (evt: ClipboardEvent | DragEvent) => {
-  const { files = [] } =
-    (evt as DragEvent).dataTransfer ??
-    (evt as ClipboardEvent).clipboardData ??
-    {};
-  if (files.length) {
-    evt.preventDefault();
-    evt.stopPropagation();
-    [...files].forEach(insertImage);
-  }
-};
-
-/* -------------------------------------------------------------------------- */
-
-const { files, open } = useFileDialog({ accept, reset });
-
-/* -------------------------------------------------------------------------- */
-
-const definitions = {
-  ...(Object.fromEntries(
-    [
-      ["upload", t("Upload Image"), open],
-      [
-        "share",
-        t("Insert Route"),
-        () => {
-          const component = VLinkDialog as Component;
-          $q.dialog({ component }).onOk((value: string) => {
-            editor.value?.runCmd("createLink", value);
-          });
-        },
-      ],
-    ].map(([icon, tip, handler]) => [icon, { handler, icon, tip }]),
-  ) as QEditorProps["definitions"]),
-  ...Object.fromEntries(
-    [
-      ...[...Array(6).keys()].map((key) => [
-        `h${String(key + 1)}`,
-        `heading${String(key + 1)}`,
-      ]),
-      ["p", "paragraph"],
-      ["code", "code"],
-    ].map(([key = "div", value]) => [
-      key,
-      {
-        htmlTip: `<span class="prose"><${key} class="!my-0">${$q.lang.editor[value as keyof StringDictionary<QuasarLanguageEditorLabel>]}</${key}></span>`,
-      },
-    ]),
-  ),
-};
-
-/* -------------------------------------------------------------------------- */
-
-defineEmits(["update:modelValue"]);
-
-watch(files, (newFiles) => {
-  if (newFiles) [...newFiles].forEach(insertImage);
-});
-
-onMounted(() => {
-  const rootElement = editor.value?.getContentEl;
-  watch(
-    () => getFonts(Fonts),
-    async (fonts) => {
-      if (rootElement) {
-        let { presets } = Defaults;
-        presets = [...presets, webFonts({ customFetch, fonts })];
-        const defaults = { presets };
-        await initUnocssRuntime({ bypassDefined, defaults, rootElement });
-      }
-    },
-    { immediate },
-  );
-  watch(
-    () => props.id,
-    async () => {
-      htm.value = await props.modelValue;
-      const contentEl = rootElement?.();
-      if (
-        contentEl?.innerHTML !== undefined &&
-        contentEl.innerHTML !== htm.value
-      )
-        contentEl.innerHTML = htm.value;
-    },
-  );
-});
-
-/* -------------------------------------------------------------------------- */
-
-const fonts = computed(() => ({
-    ...getFonts([
-      "Arial",
-      "Arial Black",
-      "Comic Sans",
-      "Courier New",
-      "Impact",
-      "Lucida Grande",
-      "Times New Roman",
-      "Verdana",
-    ]),
-    ...getFonts(Fonts),
-  })),
+  placeholder = t("Add some content to your page..."),
+  scrollTarget: Ref<Element | undefined> = ref(),
+  show = ref(false),
+  target: Ref<boolean | Element> = ref(false),
   toolbar = computed(() => [
     ["left", "center", "right", "justify"],
     ["bold", "italic", "strike", "underline", "subscript", "superscript"],
     ["hr", "link"],
-    ["print", "fullscreen"],
+    ["print", "fullscreen", "dashboard"],
     [
       ...[
         [
@@ -237,6 +158,134 @@ const fonts = computed(() => ({
     ],
     ["quote", "unordered", "ordered", "outdent", "indent"],
     ["undo", "redo"],
-    ["upload", "dashboard", "share"],
-  ]);
+    ["wallpaper", "file_present"],
+  ]),
+  tooltip = ref("");
+
+const insertImage = (file: File) => {
+    const { type } = file;
+    if (mimes.includes(type)) {
+      const filePath = `images/${uid()}.${mime.getExtension(type) ?? ""}`;
+      (async () => {
+        await putObject(
+          filePath,
+          new Uint8Array(await file.arrayBuffer()),
+          type,
+        );
+      })().catch(consoleError);
+      urls.set(filePath, URL.createObjectURL(file));
+      editor.value?.runCmd(
+        "insertHTML",
+        `<img src="${urls.get(filePath) ?? ""}" data-src="${filePath}" alt="" decoding="async" loading="lazy" />`,
+      );
+    } else $q.notify({ message });
+  },
+  listener: EventListener = ({ currentTarget, target: element }) => {
+    if (!blocks) target.value = false;
+    else if (element !== currentTarget && element instanceof Element) {
+      const { classList, tagName } = element;
+      tooltip.value = [
+        `<strong>${tagName.toLowerCase()}</strong>`,
+        ...classList,
+      ].join(".");
+      target.value = element;
+    }
+  },
+  paste = (evt: ClipboardEvent | DragEvent) => {
+    const { files = [] } =
+      (evt as DragEvent).dataTransfer ??
+      (evt as ClipboardEvent).clipboardData ??
+      {};
+    if (files.length) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      [...files].forEach(insertImage);
+    }
+  },
+  { files, open } = useFileDialog({ accept, reset });
+
+const definitions = {
+  ...(Object.fromEntries(
+    [
+      [
+        "dashboard",
+        t("Show Blocks"),
+        () => {
+          blocks = !blocks;
+          if (blocks) scrollTarget.value?.classList.add("outline");
+          else scrollTarget.value?.classList.remove("outline");
+        },
+      ],
+      ["wallpaper", t("Upload Image"), open],
+      [
+        "file_present",
+        t("Insert Route"),
+        () => {
+          const component = VLinkDialog as Component;
+          $q.dialog({ component }).onOk((value: string) => {
+            editor.value?.runCmd("createLink", value);
+          });
+        },
+      ],
+    ].map(([icon, tip, handler]) => [icon, { handler, icon, tip }]),
+  ) as QEditorProps["definitions"]),
+  ...Object.fromEntries(
+    [
+      ...[...Array(6).keys()].map((key) => [
+        `h${String(key + 1)}`,
+        `heading${String(key + 1)}`,
+      ]),
+      ["p", "paragraph"],
+      ["code", "code"],
+    ].map(([key = "div", value]) => [
+      key,
+      {
+        htmlTip: `<span class="prose"><${key} class="!my-0">${$q.lang.editor[value as keyof StringDictionary<QuasarLanguageEditorLabel>]}</${key}></span>`,
+      },
+    ]),
+  ),
+};
+
+/* -------------------------------------------------------------------------- */
+
+defineEmits(["update:modelValue"]);
+
+watch(files, (newFiles) => {
+  if (newFiles) [...newFiles].forEach(insertImage);
+});
+
+watch(target, async () => {
+  show.value = false;
+  await nextTick();
+  show.value = true;
+});
+
+watch(
+  () => props.id,
+  async () => {
+    htm.value = await props.modelValue;
+    if (scrollTarget.value && scrollTarget.value.innerHTML !== htm.value)
+      scrollTarget.value.innerHTML = htm.value;
+  },
+);
+
+onMounted(() => {
+  rootElement = editor.value?.getContentEl ?? (() => undefined);
+  scrollTarget.value = rootElement();
+  scrollTarget.value?.addEventListener("mouseover", listener);
+  editor.value?.focus();
+});
+
+onUnmounted(() => {
+  scrollTarget.value?.removeEventListener("mouseover", listener);
+});
 </script>
+
+<style scoped>
+:deep(.outline *) {
+  outline-style: dashed;
+  outline-width: 2px;
+  outline-offset: 2px;
+  outline-color: rgb(25, 118, 210);
+}
+</style>
