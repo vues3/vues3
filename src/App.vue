9e6bcc2601d496e538e1200bbd2d5d6426950684
Jerry Bruwes <jbruwes@icloud.com>
2025-02-16 18:09:52 +0200

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,98 +9,96 @@ q-layout(view="hHh Lpr lff")
       q-icon(name="wysiwyg")
       .col.text-weight-bold.text-center vueS3
       q-space
-      q-btn(@click="minimize", dense, flat, icon="minimize")
+      q-btn(dense, flat, icon="minimize", @click="minimize")
       q-btn(
-        @click="toggleMaximize",
         dense,
         flat,
-        :icon="isMaximized() ? 'unfold_less' : 'unfold_more'"
+        :icon="isMaximized() ? 'unfold_less' : 'unfold_more'",
+        @click="toggleMaximize"
       )
-      q-btn(@click="closeApp", dense, flat, icon="close")
+      q-btn(dense, flat, icon="close", @click="closeApp")
     q-toolbar
       q-toolbar-title
         q-avatar(icon="img:favicon.svg", size="xl")
         | vueS3
-      q-chip(icon="language", v-if="bucket", :label="bucket", :ripple="false")
+      q-chip(v-if="bucket", icon="language", :label="bucket", :ripple="false")
       q-btn-dropdown.q-mr-xs(
+        v-if="bucket",
         dropdown-icon="apps",
         flat,
         square,
-        stretch,
-        v-if="bucket"
+        stretch
       )
         q-list(padding)
           q-item(
-            @click="() => { click(VImportmapDialog); }",
+            v-close-popup,
             clickable,
-            v-close-popup
+            @click="() => { click(VImportmapDialog); }"
           )
             q-item-section(avatar)
               q-avatar(color="primary", icon="map", text-color="white")
             q-item-section
               q-item-label Import Map
           q-item(
-            @click="() => { click(VFaviconDialog); }",
+            v-close-popup,
             clickable,
-            v-close-popup
+            @click="() => { click(VFaviconDialog); }"
           )
             q-item-section(avatar)
               q-avatar(color="primary", icon="image", text-color="white")
             q-item-section
               q-item-label Favicon
-          q-item(
-            @click="() => { click(VRobotsDialog); }",
-            clickable,
-            v-close-popup
-          )
+          q-item(v-close-popup, clickable, @click="clickRobots")
             q-item-section(avatar)
               q-avatar(color="primary", icon="android", text-color="white")
             q-item-section
               q-item-label Robots.txt
           q-item(
-            @click="() => { click(VFontsDialog); }",
+            v-close-popup,
             clickable,
-            v-close-popup
+            @click="() => { click(VFontsDialog); }"
           )
             q-item-section(avatar)
               q-avatar(color="primary", icon="spellcheck", text-color="white")
             q-item-section
               q-item-label Fonts
-          q-item(@click="clickDomain", clickable, v-close-popup)
+          q-item(v-close-popup, clickable, @click="clickDomain")
             q-item-section(avatar)
               q-avatar(color="primary", icon="public", text-color="white")
             q-item-section
               q-item-label Domain
-          q-item(clickable, to="/", v-close-popup)
+          q-item(v-close-popup, clickable, to="/")
             q-item-section(avatar)
               q-avatar(color="primary", icon="logout", text-color="white")
             q-item-section
               q-item-label Logout
       q-btn(
-        @click="rightDrawer = !rightDrawer",
         dense,
         flat,
         icon="more_vert",
-        round
+        round,
+        @click="rightDrawer = !rightDrawer"
       )
   q-page-container.window-height
     router-view
 </template>
 
 <script setup lang="ts">
+import type { PromptInputType } from "quasar";
 import type { Component } from "vue";
 
 import "@unocss/reset/tailwind-compat.css";
+import { consoleError } from "@vues3/shared";
 import VFaviconDialog from "components/dialogs/VFaviconDialog.vue";
 import VFontsDialog from "components/dialogs/VFontsDialog.vue";
 import VImportmapDialog from "components/dialogs/VImportmapDialog.vue";
-import VRobotsDialog from "components/dialogs/VRobotsDialog.vue";
 import { useQuasar } from "quasar";
 import { domain, rightDrawer } from "stores/app";
-import { bucket } from "stores/io";
-import { useI18n } from "vue-i18n";
+import { cache, persistent } from "stores/defaults";
 // eslint-disable-next-line import-x/no-unresolved
 import "virtual:uno.css";
+import { bucket, getObjectText, putObject } from "stores/io";
+import { useI18n } from "vue-i18n";
 
 /* -------------------------------------------------------------------------- */
 
@@ -110,13 +108,7 @@ const { t } = useI18n();
 
 const $q = useQuasar(),
   cancel = true,
-  message = t("Enter a valid domain name:"),
-  persistent = true,
-  title = t("Domain");
-
-/* -------------------------------------------------------------------------- */
-
-const click = (component: Component) => {
+  click = (component: Component) => {
     $q.dialog({ component });
   },
   closeApp = () => {
@@ -134,12 +126,28 @@ const click = (component: Component) => {
     window.focusedWindowToggleMaximize();
   };
 const clickDomain = () => {
-  const model = domain.value,
-    prompt = { isValid, model };
-  $q.dialog({ cancel, message, persistent, prompt, title }).onOk(
-    (data: string) => {
-      domain.value = data;
-    },
-  );
-};
+    const message = t("Enter a valid domain name:"),
+      model = domain.value,
+      prompt = { isValid, model },
+      title = t("Domain");
+    $q.dialog({ cancel, message, persistent, prompt, title }).onOk(
+      (data: string) => {
+        domain.value = data;
+      },
+    );
+  },
+  clickRobots = async () => {
+    const message = t(
+        "Robots.txt is a text file that contains site indexing parameters for the search engine robots",
+      ),
+      title = "robots.txt",
+      model = await getObjectText(title, cache),
+      type: PromptInputType = "textarea",
+      prompt = { model, type };
+    $q.dialog({ cancel, message, persistent, prompt, title }).onOk(
+      (data: string) => {
+        putObject(title, data, "text/plain").catch(consoleError);
+      },
+    );
+  };
 </script>
