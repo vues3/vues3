f88d9aed73c41af3c637f94245359a8d0fc52bc9
Jerry Bruwes <jbruwes@icloud.com>
2025-03-17 18:08:09 +0200

    modified:   eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,48 +1,39 @@
-import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
-
 import { FlatCompat } from "@eslint/eslintrc";
 import eslint from "@eslint/js";
 import eslintPluginImportX from "eslint-plugin-import-x";
 import perfectionist from "eslint-plugin-perfectionist";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import { configs as sonarjs } from "eslint-plugin-sonarjs";
 import pluginVue from "eslint-plugin-vue";
 import tseslint, { configs, parser } from "typescript-eslint";
 import vueParser from "vue-eslint-parser";
-
-/* -------------------------------------------------------------------------- */
-
-const compat = new FlatCompat(),
-  extraFileExtensions = [".vue"],
-  ignores = ["**/dist", "**/.quasar", "**/language-configs.ts"],
-  projectService = true,
-  tsconfigRootDir = import.meta.dirname,
-  parserOptions = {
-    extraFileExtensions,
-    parser,
-    projectService,
-    tsconfigRootDir,
-  },
-  languageOptions = { parser: vueParser, parserOptions },
-  rules: FlatConfig.Rules = {
-    "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
-    "import-x/no-extraneous-dependencies": [
-      "error",
-      {
-        devDependencies: ["**/quasar.config.ts", "**/eslint.config.ts"],
-        optionalDependencies: false,
-        whitelist: ["electron"],
-      },
-    ],
-    "prettier/prettier": ["error", { endOfLine: "auto" }],
-  };
-
-/* -------------------------------------------------------------------------- */
-
+const compat = new FlatCompat();
 export default tseslint.config(
-  { ignores },
-  { rules },
-  { languageOptions },
+  { ignores: ["**/dist", "**/.quasar", "**/language-configs.ts"] },
+  {
+    rules: {
+      "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
+      "import-x/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: ["**/quasar.config.ts", "**/eslint.config.ts"],
+          optionalDependencies: false,
+          whitelist: ["electron"],
+        },
+      ],
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+    },
+  },
+  {
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        extraFileExtensions: [".vue"],
+        parser,
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
   ...compat.extends("plugin:vue-pug/vue3-recommended"),
   eslint.configs.recommended,
   eslintPluginImportX.flatConfigs.recommended,
@@ -50,7 +41,6 @@ export default tseslint.config(
   configs.strictTypeChecked,
   configs.stylisticTypeChecked,
   ...pluginVue.configs["flat/recommended"],
-  sonarjs.recommended,
   perfectionist.configs["recommended-natural"],
   eslintPluginPrettierRecommended,
 );
