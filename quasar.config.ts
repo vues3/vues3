2929b90fa620d61e6fb910e15aafee86c84b1931
Jerry Bruwes <jbruwes@icloud.com>
2025-03-17 18:08:41 +0200

    modified:   quasar.config.ts

--- quasar.config.ts
+++ quasar.config.ts
@@ -1,92 +1,64 @@
-import type { SnapOptions } from "app-builder-lib";
-import type {
-  QuasarAnimations,
-  QuasarFonts,
-  QuasarIconSets,
-  QuasarPlugins,
-} from "quasar";
-
 import { defineConfig } from "#q-app/wrappers";
 import extractorPug from "@unocss/extractor-pug";
 import { fileURLToPath } from "url";
 import { mergeConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
-
-/* -------------------------------------------------------------------------- */
-
-const extendViteConf = (viteConf: Record<string, object>) => {
-  const { define = {} } = viteConf;
-  {
-    const value = mergeConfig(define, {
-      __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
-    });
-    Reflect.defineProperty(viteConf, "define", {
-      value,
-    });
-  }
-  {
-    const value = "./";
-    Reflect.defineProperty(viteConf, "base", {
-      value,
-    });
-  }
-};
-
-/* -------------------------------------------------------------------------- */
-
-const alias = { "node:path": "path-browserify" },
-  animations: QuasarAnimations[] = ["zoomIn", "zoomOut"],
-  appId = "com.electron.vues3",
-  boot = ["main", "route", "quasar-lang-pack", "i18n", "monaco"],
-  strict = true,
-  typescript = { strict },
-  configFile = "./uno.config.ts",
-  lintCommand =
-    'eslint -c ./eslint.config.ts "./src*/**/*.{ts,js,mjs,cjs,vue}"',
-  useFlatConfig = true,
-  eslint = { lintCommand, useFlatConfig },
-  extractors = [extractorPug()],
-  include = [fileURLToPath(new URL("./src/i18n", import.meta.url))],
-  dest = "runtime",
-  src = "./node_modules/@vues3/runtime/dist/*",
-  targets = [{ dest, src }],
-  vueTsc = true,
-  vitePlugins = [
-    ["@intlify/unplugin-vue-i18n/vite", { include }],
-    ["vite-plugin-checker", { eslint, vueTsc }, { server: false }],
-    ["@unocss/vite", { configFile, extractors }],
-    [viteStaticCopy, { targets }],
-  ],
-  build: object = { alias, extendViteConf, typescript, vitePlugins },
-  grade: SnapOptions["grade"] = "stable",
-  snap = { grade },
-  builder = { appId, snap },
-  bundler = "builder",
-  css = ["app.css"],
-  open = false,
-  devServer = { open },
-  extras: (QuasarFonts | QuasarIconSets)[] = [
-    "mdi-v7",
-    "roboto-font",
-    "material-icons",
-  ],
-  plugins: (keyof QuasarPlugins)[] = ["Dialog", "Notify"],
-  framework = { plugins },
-  preloadScripts = ["electron-preload"];
-
-/* -------------------------------------------------------------------------- */
-
 export default defineConfig(() => ({
-  animations,
-  boot,
-  build,
-  css,
-  devServer,
-  electron: {
-    builder,
-    bundler,
-    preloadScripts,
+  animations: ["zoomIn", "zoomOut"],
+  boot: ["main", "route", "quasar-lang-pack", "i18n", "monaco"],
+  build: {
+    alias: { "node:path": "path-browserify" },
+    extendViteConf: (viteConf) => {
+      const { define = {} } = viteConf;
+      Reflect.defineProperty(viteConf, "define", {
+        value: mergeConfig(define, {
+          __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+        }),
+      });
+      Reflect.defineProperty(viteConf, "base", {
+        value: "./",
+      });
+    },
+    typescript: { strict: true },
+    vitePlugins: [
+      [
+        "@intlify/unplugin-vue-i18n/vite",
+        { include: [fileURLToPath(new URL("./src/i18n", import.meta.url))] },
+      ],
+      [
+        "vite-plugin-checker",
+        {
+          eslint: {
+            lintCommand:
+              'eslint -c ./eslint.config.ts "./src*/**/*.{ts,js,mjs,cjs,vue}"',
+            useFlatConfig: true,
+          },
+          vueTsc: true,
+        },
+        { server: false },
+      ],
+      [
+        "@unocss/vite",
+        { configFile: "./uno.config.ts", extractors: [extractorPug()] },
+      ],
+      [
+        // @ts-expect-error Plugin<any>[]
+        viteStaticCopy,
+        {
+          targets: [
+            { dest: "runtime", src: "./node_modules/@vues3/runtime/dist/*" },
+          ],
+        },
+      ],
+    ],
   },
-  extras,
-  framework,
+  css: ["app.css"],
+  devServer: { open: false },
+  electron: {
+    builder: { appId: "com.electron.vues3", snap: { grade: "stable" } },
+    bundler: "builder",
+    preloadScripts: ["electron-preload"],
+  },
+  extras: ["mdi-v7", "roboto-font", "material-icons"],
+  framework: { plugins: ["Dialog", "Notify"] },
 }));
