02468626cdba6ac2ab644aa9423dac585f089e59
Jerry Bruwes <jbruwes@icloud.com>
2025-05-14 16:44:38 +0200

    modified:   src-electron/electron-main.ts

--- src-electron/electron-main.ts
+++ src-electron/electron-main.ts
@@ -1,47 +1,32 @@
 import { enable, initialize } from "@electron/remote/main/index.js";
-import { consoleError } from "@vues3/shared";
 import { app, BrowserWindow, Menu } from "electron";
 import path from "path";
 import { fileURLToPath } from "url";
 
-/* -------------------------------------------------------------------------- */
-
 let mainWindow: BrowserWindow | undefined;
 
-/* -------------------------------------------------------------------------- */
-
-const currentDir = fileURLToPath(new URL(".", import.meta.url)),
-  devTools = false,
-  frame = false,
-  height = 600,
-  icon = path.resolve(currentDir, "icons/icon.png"),
-  preload = path.resolve(
-    currentDir,
-    path.join(
-      process.env.QUASAR_ELECTRON_PRELOAD_FOLDER,
-      `electron-preload${process.env.QUASAR_ELECTRON_PRELOAD_EXTENSION}`,
-    ),
-  ),
-  sandbox = false,
-  show = false,
-  webPreferences = { devTools, preload, sandbox },
-  width = 1000;
-
-/* -------------------------------------------------------------------------- */
+const currentDir = fileURLToPath(new URL(".", import.meta.url));
 
 const createWindow = async () => {
   mainWindow = new BrowserWindow({
-    frame,
-    height,
-    icon,
-    show,
-    webPreferences,
-    width,
+    icon: path.resolve(currentDir, "icons/icon.png"),
+    show: false,
+    webPreferences: {
+      devTools: false,
+      preload: path.resolve(
+        currentDir,
+        path.join(
+          process.env.QUASAR_ELECTRON_PRELOAD_FOLDER,
+          `electron-preload${process.env.QUASAR_ELECTRON_PRELOAD_EXTENSION}`,
+        ),
+      ),
+      sandbox: false,
+    },
+    width: 1000,
   });
   enable(mainWindow.webContents);
-  if (process.env.DEV)
-    await mainWindow.loadURL(process.env.APP_URL).catch(consoleError);
-  else await mainWindow.loadFile("index.html").catch(consoleError);
+  if (process.env.DEV) await mainWindow.loadURL(process.env.APP_URL);
+  else await mainWindow.loadFile("index.html");
   mainWindow.on("closed", () => {
     mainWindow = undefined;
   });
@@ -49,14 +34,12 @@ const createWindow = async () => {
   // mainWindow.webContents.openDevTools();
 };
 
-/* -------------------------------------------------------------------------- */
-
 initialize();
 Menu.setApplicationMenu(null);
-app.whenReady().then(createWindow).catch(consoleError);
+void app.whenReady().then(createWindow);
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
 app.on("activate", () => {
-  if (mainWindow === undefined) createWindow().catch(consoleError);
+  if (mainWindow === undefined) void createWindow();
 });
